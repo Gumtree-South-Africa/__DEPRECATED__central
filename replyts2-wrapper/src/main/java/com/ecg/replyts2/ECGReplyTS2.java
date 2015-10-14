193b26e264ca9fb6d03634a172dc317a1b364a8e
Rene Weber <reweber@ebay.com>
2015-10-14 11:57:19 +0200

    Created wrapper for reply ts + added message center as sub module

---
+++ replyts2-wrapper/src/main/java/com/ecg/replyts2/ECGReplyTS2.java
@@ -0,0 +1,14 @@
+package com.ecg.replyts2;
+
+import com.ecg.replyts.core.runtime.ReplyTS;
+
+/**
+ * Created by reweber on 14/10/15
+ */
+public class ECGReplyTS2 {
+
+    public static void main(String[] args) throws Exception {
+        ReplyTS.main(args);
+    }
+
+}
