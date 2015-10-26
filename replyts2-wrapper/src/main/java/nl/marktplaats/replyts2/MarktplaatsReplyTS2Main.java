06bc4ccff5dc99b5ee388adb80dbfacdf024e888
Erik van Oosten <evanoosten@ebay.com>
2015-10-26 17:07:35 +0100

    Added RTS2-core (cassandra branch), small changes to generated tar file.

--- replyts2-wrapper/src/main/java/nl/marktplaats/replyts2/MarktplaatsReplyTS2Main.java
+++ replyts2-wrapper/src/main/java/nl/marktplaats/replyts2/MarktplaatsReplyTS2Main.java
@@ -1,11 +1,8 @@
-package com.ecg.replyts2;
+package nl.marktplaats.replyts2;
 
 import com.ecg.replyts.core.runtime.ReplyTS;
 
-/**
- * Created by reweber on 14/10/15
- */
-public class ECGReplyTS2 {
+public class MarktplaatsReplyTS2Main {
 
     public static void main(String[] args) throws Exception {
         ReplyTS.main(args);
