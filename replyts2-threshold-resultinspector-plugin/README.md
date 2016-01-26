b6d35811e9f81a8d1c33875ba36ace69eb474a5c
Geert Gerritsen <ggerritsen@ebay.com>
2016-01-26 11:54:57 +0100

    Desubmodularized threshold-resultinspector-plugin (using the submodule's 'marktplaats' branch)

---
+++ replyts2-threshold-resultinspector-plugin/README.md
@@ -0,0 +1,25 @@
+# replyts2-threshold-resultinspector-plugin
+Migration of the ReplyTS 1 result inspector  plugin to ReplyTS 2. Made by eBay Kleinanzeigen.
+
+The result inspector has thresholds for putting incoming messages to HELD and BLOCKED. It will sum up the score output of all filters applied to the message and then compare with the thresholds. 
+
+# Filter configuration
+
+
+configurations should look like this:
+```
+{
+    held: 200, 
+    blocked: 600
+}
+```
+
+Putting configuration into ReplyTS example: 
+```
+curl -X PUT http://localhost:8081/configv2/com.ecg.de.kleinanzeigen.replyts.thresholdresultinspector.ThresholdResultInspectorFactory/Default -H "Content-Type: application/json" -d '
+    {  "priority": 100, 
+       "state": "ENABLED", 
+       "configuration": {"held": 75, "blocked": 300 } }'
+```
+# Contributor
+Matthias Huttar from eBay Kleinanzeigen Germany: mhuttar@ebay-kleinanzeigen.de
---
+++ replyts2-threshold-resultinspector-plugin/src/test/java/com/ecg/de/kleinanzeigen/replyts/thresholdresultinspector/ThresholdResultInspectorAutomationTest.java
@@ -0,0 +1,45 @@
+package com.ecg.de.kleinanzeigen.replyts.thresholdresultinspector;
+
+import com.ecg.replyts.core.api.model.conversation.MessageState;
+import com.ecg.replyts.core.api.util.JsonObjects;
+import com.ecg.replyts.integration.test.AwaitMailSentProcessedListener;
+import com.ecg.replyts.integration.test.MailBuilder;
+import com.ecg.replyts.integration.test.ReplyTsIntegrationTestRule;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ThresholdResultInspectorAutomationTest {
+
+    @Rule
+    public ReplyTsIntegrationTestRule rule = new ReplyTsIntegrationTestRule();
+
+
+
+    @Before
+    public void setUp() throws Exception {
+        rule.registerConfig((Class)ThresholdResultInspectorFactory.class, (ObjectNode) JsonObjects.parse("{'held':50, 'blocked':100}"));
+    }
+
+    @Test
+    public void allowsMessageIfNoScore() throws Exception {
+        AwaitMailSentProcessedListener.ProcessedMail processed = rule.deliver(MailBuilder.aNewMail().adId("12").from("foo@bar.com").to("bar@foo.com").subject("as").htmlBody("asdf"));
+
+        assertEquals(MessageState.SENT, processed.getMessage().getState());
+    }
+
+    @Test
+    public void putsMessageOnBlockedIfFilterScoresExceeds() throws Exception {
+
+        rule.registerConfig(ScoringFilterFactory.class, JsonObjects.builder().attr("score", 100).build());
+
+
+        AwaitMailSentProcessedListener.ProcessedMail processed = rule.deliver(MailBuilder.aNewMail().adId("12").from("foo@bar.com").to("bar@foo.com").subject("as").htmlBody("asdf"));
+
+        assertEquals(MessageState.BLOCKED, processed.getMessage().getState());
+
+    }
+}
