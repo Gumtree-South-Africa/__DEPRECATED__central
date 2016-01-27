c63e7d5310d1d8e0d37edf68df3c4ee563605500
Geert Gerritsen <ggerritsen@ebay.com>
2016-01-27 10:26:30 +0100

    Make sure we setup cassandra schema for messagebox & volume filter as well

--- setup-cassandra.sh
+++ setup-cassandra.sh
@@ -3,3 +3,5 @@
 # If necessary, download cassandra from http://archive.apache.org/dist/cassandra/2.1.11/
 
 /opt/apache-cassandra-2.1.11/bin/cqlsh replyts.dev.kjdev.ca 9042 -f core-runtime/src/main/resources/cassandra_schema.cql
+/opt/apache-cassandra-2.1.11/bin/cqlsh replyts.dev.kjdev.ca 9042 -f replyts2-mp-plugins/replyts2-filter-volume/src/main/resources/cassandra_volume_filter_schema.cql
+/opt/apache-cassandra-2.1.11/bin/cqlsh replyts.dev.kjdev.ca 9042 -f cassandra_messagebox_schema.cql
