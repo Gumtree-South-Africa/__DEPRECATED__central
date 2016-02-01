c0c3986102085db65aa804a47bf5507a524f3d89
Geert Gerritsen <ggerritsen@ebay.com>
2016-02-01 14:23:45 +0100

    Fix time reporting in build script

--- bin/build.sh
+++ bin/build.sh
@@ -6,10 +6,10 @@ function log() {
 
 function main() {
 	local start=$(date +"%s")
-	local end=$(date +"%s")
 
 	mvn -s etc/settings.xml clean package
 
+	local end=$(date +"%s")
 	local diff=$(($end-$start))
 	local time=$(printf "Total time: %d min %d sec" $(($diff / 60)) $(($diff % 60)))
 	log $time
