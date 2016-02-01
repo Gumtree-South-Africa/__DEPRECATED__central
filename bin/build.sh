4896db3098208127b6c74449232ac05b99aae8cc
Geert Gerritsen <ggerritsen@ebay.com>
2016-02-01 14:05:54 +0100

    Improve build script with logging & build time

--- bin/build.sh
+++ bin/build.sh
@@ -1,2 +1,18 @@
 #!/usr/bin/env bash
-mvn -s etc/settings.xml clean package
+
+function log() {
+	echo "[$(date)]: $*"
+}
+
+function main() {
+	local start=$(date +"%s")
+	local end=$(date +"%s")
+
+	mvn -s etc/settings.xml clean package
+
+	local diff=$(($end-$start))
+	local time=$(printf "Total time: %d min %d sec" $(($diff / 60)) $(($diff % 60)))
+	log $time
+}
+
+main
