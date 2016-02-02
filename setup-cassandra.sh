0b44bcf082a000779c2137279ee2e8e5966e46e1
Dave Stevens <davstevens@ebay.com>
2016-02-02 15:48:48 +0100

    github sync test
    Change-Id: Id2d74da586a720489e783c4ef4a6b4c2da370234
    Reviewed-on: https://gerrit.ecg.so/30101
    Reviewed-by: Dave Stevens <davstevens@ebay.com>
    Tested-by: Dave Stevens <davstevens@ebay.com>

--- setup-cassandra.sh
+++ setup-cassandra.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# If necessary, download cassandra from http://archive.apache.org/dist/cassandra/2.1.11/
+# If necessary, download Cassandra from http://archive.apache.org/dist/cassandra/2.1.11/
 
 /opt/apache-cassandra-2.1.11/bin/cqlsh replyts.dev.kjdev.ca 9042 -f core-runtime/src/main/resources/cassandra_schema.cql
 /opt/apache-cassandra-2.1.11/bin/cqlsh replyts.dev.kjdev.ca 9042 -k replyts2 -f replyts2-mp-plugins/replyts2-filter-volume/src/main/resources/cassandra_volume_filter_schema.cql
