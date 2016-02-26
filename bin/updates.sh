a60b69a1ecb1f10ef13d8e8935fab3f0aae89bda
Jasper van Veghel <jvanveghel@ebay.com>
2016-02-26 17:35:15 +0100

    CASINT-1383: Also added in the core-{api,runtime} modules
    Change-Id: Ifc626c62c4cabb9f26eed7880e6a866cfeb939f2
    Reviewed-on: https://gerrit.ecg.so/30987
    Tested-by: Gerrit-At-Gitmaster001
    Reviewed-by: Geert Gerritsen <ggerritsen@ebay.com>

---
+++ bin/updates.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+readonly DIR=$(dirname $0)/..
+
+function log() {
+	echo "[$(date)]: $*"
+}
+
+function fatal() {
+	log $*
+	exit 1
+}
+
+function main() {
+	for i in $DIR/core-{api,runtime}/README.md $DIR/{filters,postprocessors,messagecenters,integration-tests}/*/README.md ; do
+		IDIR=$(basename `dirname $i`)
+
+		MATCH=( `sed -n -e 's/.*\(https:\/\/github.*$\)/\1/p;s/.* hash: \(.*\))$/\1/p' $i` )
+		HEAD=`git ls-remote ${MATCH[0]} HEAD | cut -d$'\t' -f1`
+
+		if ! [ -z "${MATCH[1]:-}" ] && ! [ -z "$HEAD" ] ; then
+			printf "Checking %-30s  ..\t[ %s ]\n" "$IDIR" \
+			  $([[ "$HEAD" == "${MATCH[1]}" ]] && echo "OK" || echo "OUTDATED")
+		fi
+	done
+}
+
+main
