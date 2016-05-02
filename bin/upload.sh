71dc031c3f484a4a03f74548a4489558bf9afa67
Geert Gerritsen <ggerritsen@ebay.com>
2016-05-02 15:54:34 +0200

    Add upload script.
    Change-Id: I08ffac83afc22f69282ff2310a30561f92b14042
    Reviewed-on: https://gerrit.ecg.so/34201
    Tested-by: Gerrit-At-Gitmaster001
    Reviewed-by: Geert Gerritsen <ggerritsen@ebay.com>

---
+++ bin/upload.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# upload.sh takes a package and uploads it to the tenant's deploy server (e.g. https://autodeploy.mobile.corp.de)
+
+set -o nounset
+set -o errexit
+
+# map to lookup the upload hosts for a tenant
+declare -A HOSTS=(
+  ["ebayk"]="https://comaas-uploader:ohy9Te#hah9U@kautodeploy.corp.mobile.de/storage/belen-productive-deployment-releases/ecg/comaas/" \
+  ["mde"]="https://comaas-uploader:ohy9Te#hah9U@autodeploy.corp.mobile.de/storage/belen-productive-deployment-releases/ecg/comaas/"
+)
+
+# package of the form distribution-456abf9-ebayk-prod.tar.gz
+readonly PACKAGE=$1
+readonly TENANT=$(echo ${PACKAGE} | cut -d'-' -f3)
+
+# host to upload to
+readonly HOST=${HOSTS[$TENANT]}
+readonly URL=$(echo ${HOST} | cut -d'@' -f2)
+
+echo "Uploading $PACKAGE to $URL"
+readonly start=$(date +"%s")
+
+set -o xtrace
+curl -vvv --progress-bar -X PUT --upload-file "${PACKAGE}" "${HOST}" > /dev/null
+set +o xtrace
+
+readonly end=$(date +"%s")
+readonly diff=$(($end-$start))
+readonly time=$(printf "%d min %d sec" $(($diff / 60)) $(($diff % 60)))
+
+echo "Finished uploading $PACKAGE to $URL in $time."
