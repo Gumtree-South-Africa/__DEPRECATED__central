67f82ffb554e3a7c4727346f7ec6305cc8b39d5b
Jasper van Veghel <jvanveghel@ebay.com>
2016-05-24 14:23:39 +0200

    Fixed a compilation issue due to a merge conflict
    Change-Id: I23085c3557f0804a08fae678bef82bd3c8264ed3
    Reviewed-on: https://gerrit.ecg.so/35289
    Tested-by: Gerrit-At-Gitmaster001
    Reviewed-by: Jasper Van Veghel <jvanveghel@ebay.com>

--- core-runtime/src/main/java/com/ecg/replyts/core/runtime/ReplyTS.java
+++ core-runtime/src/main/java/com/ecg/replyts/core/runtime/ReplyTS.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
