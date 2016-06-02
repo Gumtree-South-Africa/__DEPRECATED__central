bfcba0bd7ae5556fcadca5adc3e121da3006331d
Geert Gerritsen <ggerritsen@ebay.com>
2016-06-02 14:26:33 +0200

    Add --logdir to not let deploy.py log in /opt/log
    Change-Id: Ie10235b535687a1f0c11c43b6724274a5f64d1a0
    Reviewed-on: https://gerrit.ecg.so/35877
    Tested-by: Gerrit-At-Gitmaster001
    Reviewed-by: Geert Gerritsen <ggerritsen@ebay.com>

--- bin/distribute.sh
+++ bin/distribute.sh
@@ -25,7 +25,7 @@ for PKG in $(ls $BUILD_DIR/comaas-$TENANT*); do
   if [[ "$TENANT" == "mp" && "$DESTINATION" == "demo" ]] ; then
     tar -xzf ${PKG} --to-command="cat" ./conf/deploy.conf > deploy.conf
     # This requires the deploy.py script to be on the PATH
-    deploy.py --redeploy --config deploy.conf --component ${PKG} --dry-run
+    deploy.py --redeploy --config deploy.conf --logdir . --component ${PKG} --dry-run
   else
     `dirname $0`/upload.sh $TENANT $DESTINATION $GIT_HASH $PKG
   fi
