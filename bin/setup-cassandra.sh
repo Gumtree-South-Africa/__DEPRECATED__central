ed87eb088f01368f36a8f76a6020940ab3de268b
Geert Gerritsen <ggerritsen@ebay.com>
2016-06-02 12:30:55 +0200

    Setup-cassandra.sh: use cqlsh on $PATH
    Change-Id: Ifeb6cf0f7da801e51826f66b94298fe67d8e6bb7
    Reviewed-on: https://gerrit.ecg.so/35868
    Tested-by: Gerrit-At-Gitmaster001
    Reviewed-by: Geert Gerritsen <ggerritsen@ebay.com>

--- bin/setup-cassandra.sh
+++ bin/setup-cassandra.sh
@@ -13,32 +13,24 @@ if [ -z "$KEYSPACE" ] ; then
     KEYSPACE="replyts2"
 fi
 
-if [ -z "$CQLSH_PATH" ] ; then
-    shopt -s nullglob
-
-    FILES=(/opt/apache-cassandra* /opt/cassandra*)
- 
-    if [ -d "${FILES[0]}/bin" ] ; then
-      CQLSH_PATH="${FILES[0]}/bin"
-    else
-       : ${CQLSH_PATH:?"Please set the path to cqlsh e.g. /opt/apache-cassandra-2.1.13/bin"}
+if [ -z "$(which cqlsh)" ] ; then
+    export PATH=$PATH:/opt/cassandra/bin:/usr/sbin
+    if [ -z "$(which cqlsh)" ] ; then
+        echo "Could not find cqlsh on PATH"
+        exit 1
     fi
- else
-    : ${CQLSH_PATH:?"Please set the path to cqlsh e.g. /opt/apache-cassandra-2.1.13/bin"}
- fi
-  
-echo "Using CQLSH_PATH = $CQLSH_PATH"
+fi
 
 # Don't fail with an error - this is typically desirable behavior
-$CQLSH_PATH/cqlsh $HOST 9042 -e "USE system; SELECT keyspace_name FROM schema_keyspaces WHERE keyspace_name = '$KEYSPACE';" | grep '0 rows' >/dev/null ||
+cqlsh $HOST 9042 -e "USE system; SELECT keyspace_name FROM schema_keyspaces WHERE keyspace_name = '$KEYSPACE';" | grep '0 rows' >/dev/null ||
   echo "Keyspace $KEYSPACE already exists - not recreating it" && exit 0
 
-# $CQLSH_PATH/cqlsh $HOST 9042 -e "DROP KEYSPACE IF EXISTS $KEYSPACE;"
+# cqlsh $HOST 9042 -e "DROP KEYSPACE IF EXISTS $KEYSPACE;"
 
-$CQLSH_PATH/cqlsh $HOST 9042 -e "CREATE KEYSPACE $KEYSPACE WITH replication = {'class': 'NetworkTopologyStrategy', 'ams1': '2'} AND durable_writes = true;"
-$CQLSH_PATH/cqlsh $HOST 9042 -k "$KEYSPACE" -f core-runtime/src/main/resources/cassandra_schema.cql
-$CQLSH_PATH/cqlsh $HOST 9042 -k "$KEYSPACE" -f filters/mp-filter-volume/src/main/resources/cassandra_volume_filter_schema.cql
-$CQLSH_PATH/cqlsh $HOST 9042 -k "$KEYSPACE" -f messagecenters/mp-messagecenter/src/main/resources/cassandra_messagebox_schema.cql
+cqlsh $HOST 9042 -e "CREATE KEYSPACE $KEYSPACE WITH replication = {'class': 'NetworkTopologyStrategy', 'ams1': '2'} AND durable_writes = true;"
+cqlsh $HOST 9042 -k "$KEYSPACE" -f core-runtime/src/main/resources/cassandra_schema.cql
+cqlsh $HOST 9042 -k "$KEYSPACE" -f filters/mp-filter-volume/src/main/resources/cassandra_volume_filter_schema.cql
+cqlsh $HOST 9042 -k "$KEYSPACE" -f messagecenters/mp-messagecenter/src/main/resources/cassandra_messagebox_schema.cql
 
 if [ $? -eq 0 ] ; then
     echo "Keyspace $KEYSPACE was successfully created"
