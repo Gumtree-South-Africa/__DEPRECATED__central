183edfdf95af2b57c3a506f9edf009056fb3e570
ptroshin <ptroshin@ebay.com>
2016-07-22 16:26:21 +0200

    COMAAS-150 run-locally script changes
    * Where's this bare package coming from?
    Run locally conviguration is against vagrant whilst bare is a bare minimum configuration just to start comaas
    Its less functional and serves different purpose.
    
    * We do not run using build.sh script because the control flow is very different
    between the two (run-locally and build.sh) and I did not want to overcomplicate build.sh
    especially considering that run-locally is very narrow use case unlikely to come useful for the developers
    
    Change-Id: I629dff34a6b5af30ec6abff8a26b0d4582035356
    Reviewed-on: https://gerrit.ecg.so/38661
    Tested-by: Gerrit-At-Gitmaster001
    Tested-by: Comaas Jenkins <DL-eCG-comaas-dev@ebay.com>
    Reviewed-by: Geert Gerritsen <ggerritsen@ebay.com>
    Reviewed-by: Peter Troshin <ptroshin@ebay.com>

--- bin/run-locally.sh
+++ bin/run-locally.sh
@@ -12,7 +12,8 @@ readonly CASSANDRA_DIR="$PWD/../cassandra_tmp"
 readonly CASSANDRA_PID="$PWD/cassandra.pid"
 readonly CASSANDRA_HOME=$CASSANDRA_DIR
 readonly PORT=18081
-readonly ATTEMPTS=10
+readonly ATTEMPTS=30
+readonly HEALTH_CHECK_DELAY=3
 readonly HOST='localhost'
 
 function parseCmd() {
@@ -57,17 +58,16 @@ function stopCassandra() {
 }
 
 function startComaas() {
-    # stop & clean cassandra dir on exit
+    # stop comaas on exit
     trap "stopComaas" EXIT
     log "Starting Comaas"
     cd distribution/target
     tar xfz distribution-$TENANT-bare.tar.gz
     cd distribution
-    echo "Writing comaas output to ${COMAAS_OUT}"
+    log "Writing comaas output to ${COMAAS_OUT}"
     bin/comaas > ${COMAAS_OUT} 2>&1 &
     echo $! > ${COMAAS_PID}
-    CPID=`cat ${COMAAS_PID}`
-    log "Comaas pid: $CPID"
+    log "Comaas pid: $(cat ${COMAAS_PID})"
 }
 
 function stopComaas() {
@@ -87,7 +87,7 @@ function stopComaas() {
         done
         if $(ps -p "$PID" > /dev/null); then
             log "Comaas didn't exit in 10 seconds, killing it. You may want to check if cassandra is still running"
-            kill ${PID}
+            kill -9 ${PID}
         fi
         rm ${COMAAS_PID}
         log "Comaas stopped"
@@ -101,27 +101,30 @@ function log() {
 function main() {
    log "Starting comaas for tenant $TENANT"
    local start=$(date +"%s")
-   startCassandra
-   sleep 5
-   $DIR/setup-cassandra.sh localhost replyts2
+
+   if [[ "$TENANT" == "mp" ]] ; then
+       startCassandra
+       sleep 5
+       $DIR/setup-cassandra.sh localhost replyts2
+   fi
 
    startComaas
-   sleep 6
-
+   sleep 10
 
    for i in $(seq 1 $ATTEMPTS) ; do
-       sleep 2
+       sleep "$HEALTH_CHECK_DELAY"
        HEALTH=$(curl -s http://${HOST}:${PORT}/health)
-       echo "${HOST}'s health is $HEALTH"
 
-       if [ -n "$HEALTH" ]; then
-           log "Waiting for comaas to start."
-           break
+       log "Waiting for comaas to start. Waited for $(($HEALTH_CHECK_DELAY * $i))s"
+
+       if [ ! -z "$HEALTH" ]; then
+           echo "${HOST}'s health is $HEALTH"
+	   break
        fi
 
        if [ $i -eq $ATTEMPTS ]; then
           echo "Unable to get health from http://${HOST}:${PORT}/health, exiting"
-
+          exit 1
        fi
    done
 
@@ -130,11 +133,14 @@ function main() {
        echo "Host ${HOST} is running the correct tenant: $ACTUAL_TENANT"
    else
        echo "Host ${HOST} is NOT running the correct version: $ACTUAL_TENANT (should be running $TENANT)"
+       exit 1
    fi
 
    stopComaas
-   stopCassandra
-   sleep 3
+
+   if [[ "$TENANT" == "mp" ]] ; then
+       stopCassandra
+   fi
 
    local end=$(date +"%s")
    local diff=$(($end-$start))
@@ -145,13 +151,12 @@ function main() {
 function usage() {
 cat << EOF
 Usage:
-    no args (default) - build all modules
-    -t - builds and tests (but not integration tests) for Comaas and tenants
-
-    Examples: "$0 -t -T ebayk,mp " - build and test ebayk and mp distributions
+    $0 $TENANT
+    Run Comaas for TENANT where TENANT one of ebayk,mp,mde,kjca,gtau
 
 EOF
 }
 
 parseCmd ${ARGS}
 main
+
