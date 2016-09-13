4fbc1ecfad5ac85c317f3255e2de7d2b0cfa7b53
aneacsu <aneacsu@marktplaats.nl>
2016-09-13 15:12:54 +0200

    Improve get postbox test
    Change-Id: I0466a93c45b8e7826be17db29454d9fec286e3fa
    Reviewed-on: https://gerrit.ecg.so/42250
    Tested-by: Gerrit-At-Gitmaster001
    Reviewed-by: Samuel Fernandez Munoz <smunoz@marktplaats.nl>
    Reviewed-by: Gerrit-At-Gitmaster001

--- messagecenters/mp-messagecenter/src/test/java/com/ecg/messagecenter/webapi/PostBoxControllerAcceptanceTest.java
+++ messagecenters/mp-messagecenter/src/test/java/com/ecg/messagecenter/webapi/PostBoxControllerAcceptanceTest.java
@@ -43,16 +43,22 @@ public class PostBoxControllerAcceptanceTest {
 
     @Test
     public void getPostBox() {
-        testRule.deliver(MAIL1);
+        String convId1 = testRule.deliver(MAIL1).getConversation().getId();
+        testRule.waitForMail();
+        String convId2 = testRule.deliver(MAIL2).getConversation().getId();
         testRule.waitForMail();
 
         RestAssured.given()
                 .expect()
                 .statusCode(200)
-                .body("body.conversations.size()", equalTo(1))
-                .body("body.numUnread", equalTo(1))
+                .body("body.conversations.size()", equalTo(2))
+                .body("body.numUnread", equalTo(2))
+                .body("body.conversations[0].id", equalTo(convId2))
                 .body("body.conversations[0].adId", equalTo("232323"))
-                .body("body.conversations[0].textShortTrimmed", equalTo("first contact from buyer 1"))
+                .body("body.conversations[0].textShortTrimmed", equalTo("first contact from buyer 3"))
+                .body("body.conversations[1].id", equalTo(convId1))
+                .body("body.conversations[1].adId", equalTo("232323"))
+                .body("body.conversations[1].textShortTrimmed", equalTo("first contact from buyer 1"))
                 .get("http://localhost:" + testRule.getHttpPort() + "/msgcenter/postboxes/2");
     }
 
