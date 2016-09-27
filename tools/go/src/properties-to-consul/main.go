5b7f26c30ed22818c69e849a05fa4338e706ccb9
Geert Gerritsen <ggerritsen@ebay.com>
2016-09-27 21:58:52 +0200

    COMAAS-213 Added the properties-to-consul tool.
    Thanks goes to Roman, since we copied & modified the tool he wrote to
    do this for iCAS.
    
    * Puts properties from a file under a certain name into the Consul KV
      store
    * Might need to move to a different location (i.e. not in this repo),
      depending on when we want to put properties into Consul during
      deployment
    * No support for removing properties, but we probably don't need this
    * Does not check if the properties were set to the correct values
    
    Change-Id: I992493161ccd8a06a503ba5df8ddbc439527e284
    Reviewed-on: https://gerrit.ecg.so/43407
    Tested-by: Gerrit-At-Gitmaster001
    Tested-by: Comaas Jenkins <DL-eCG-comaas-dev@ebay.com>
    Reviewed-by: Remmelt Pit <rpit@ebay.com>

---
+++ tools/go/src/properties-to-consul/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/rickar/props"
+)
+
+func readProperties(fileName string) (*props.Properties, error) {
+	log.Println("Reading properties from file replyts.properties")
+	reader, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	return props.Read(reader)
+}
+
+func main() {
+	consulAddress := flag.String("consul", "http://localhost:8500/", "consul host to connect to")
+	prefix := flag.String("prefix", "", "prefix or directory in consul, properties will be saved to consuladdress/v1/kv/prefix/...")
+	filename := flag.String("file", "replyts.properties", "the properties file to read")
+	flag.Parse()
+
+	*consulAddress += "/v1/kv/"
+
+	properties, err := readProperties(*filename)
+	if err != nil {
+		log.Fatalf("Encountered an error while reading input file %s: %s", *fileName, err)
+	}
+	names := properties.Names()
+
+	client := &http.Client{}
+	u, err := url.Parse(*consulAddress + *prefix)
+	if err != nil {
+		log.Fatalf("Encountered an error parsing Consul URL %s: %s", *consulAddress, err)
+	}
+	p := path.Clean(u.RequestURI())
+
+	log.Printf("Importing %d properties into Consul at %s", len(names), u.String())
+
+	for _, name := range names {
+		u.Path = p + "/" + name
+		urlStr := u.String()
+
+		res, err := http.Get(urlStr)
+		if err != nil {
+			log.Fatalf("Encountered an error requesting %q: %s", urlStr, err)
+		}
+
+		if res.StatusCode != http.StatusOK && res.StatusCode != http.StatusNotFound {
+			log.Fatalf("Failed to query Consul for status, received %d: %s", res.StatusCode, urlStr)
+		}
+
+		if res.StatusCode == http.StatusOK {
+			//This property already exists, so let's 'check-and-set' to avoid race conditions
+			v := url.Values{}
+			v.Set("cas", res.Header.Get("X-Consul-Index"))
+			u.RawQuery = v.Encode()
+		}
+
+		urlStr = u.String()
+		req, err := http.NewRequest("PUT", urlStr, strings.NewReader(properties.Get(name)))
+		if err != nil {
+			log.Fatalf("Encountered an error creating http request for %q: %s", urlStr, err)
+		}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatalf("Encountered an error reading %q: %s", urlStr, err)
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			log.Printf("[ERROR] PUT returned status code != 200: %d for %q\n%q", resp.StatusCode, urlStr, resp)
+		}
+	}
+}
