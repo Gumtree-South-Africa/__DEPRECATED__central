95cdb8ca6ad9c491c3bd473e994eb6acca56d479
Geert Gerritsen <ggerritsen@ebay.com>
2016-12-07 09:22:42 +0100

    COMAAS-291 Add upload-packages-to-repos.sh
    Change-Id: Ica7ded219078c78c587e60928fadb726385f2afc
    Reviewed-on: https://gerrit.ecg.so/46899
    Tested-by: Gerrit-At-Gitmaster001
    Reviewed-by: Geert Gerritsen <ggerritsen@ebay.com>

---
+++ bin/upload-packages-to-repos.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+# Nomad packages:
+./bin/upload_ecg_repos.sh ${TENANT} ${BUILD_DIR}/comaas-${TENANT}-comaasqa-${TIMESTAMP}-${GIT_HASH}-nomad.tar.gz ${TIMESTAMP} sandbox
+./bin/upload_ecg_repos.sh ${TENANT} ${BUILD_DIR}/comaas-${TENANT}-sandbox-${TIMESTAMP}-${GIT_HASH}-nomad.tar.gz ${TIMESTAMP} sandbox
+
+
+# properties:
+
+# Create a zip with all the tenant's configuration to be imported into Consul
+ARTIFACT_NAME="comaas-${TENANT}-configuration-${TIMESTAMP}-${GIT_HASH}.tar.gz"
+tar zcf ${BUILD_DIR}/${ARTIFACT_NAME} -C distribution/conf/${TENANT} ./import_into_consul
+echo "Created ${BUILD_DIR}/${ARTIFACT_NAME}"
+
+# upload
+./bin/upload_ecg_repos.sh ${TENANT} ${BUILD_DIR}/${ARTIFACT_NAME} ${TIMESTAMP} sandbox
+./bin/upload_ecg_repos.sh ${TENANT} ${BUILD_DIR}/${ARTIFACT_NAME} ${TIMESTAMP} prod
+
+
+# sandbox 'deployer' package:
+
+# unpack the comaasqa package
+tar xf distribution/target/distribution-${TENANT}-comaasqa.tar.gz -C distribution/target
+
+# Create a package for sandbox
+PACKAGE_NAME="comaas-${TENANT}_sandbox-${TIMESTAMP}-${GIT_HASH}"
+ARTIFACT_NAME="${PACKAGE_NAME}.tar.gz"
+rm -rf distribution/target/distribution/conf
+mkdir -p distribution/target/distribution/conf-${TENANT}
+cp distribution/conf/${TENANT}/sandbox/* distribution/target/distribution/conf-${TENANT}
+sed -i.bak "s~/conf ~/conf-${TENANT} ~" distribution/target/distribution/bin/comaas
+mv distribution/target/distribution distribution/target/${PACKAGE_NAME}
+tar czf ${BUILD_DIR}/${ARTIFACT_NAME} -C distribution/target/ ${PACKAGE_NAME}
+rm -rf distribution/target/${PACKAGE_NAME}
+echo "Created ${BUILD_DIR}/${ARTIFACT_NAME}"
+
+# upload
+./bin/upload_ecg_repos.sh ${TENANT} ${BUILD_DIR}/${ARTIFACT_NAME} ${TIMESTAMP} sandbox
+
+
+# production 'deployer' package:
+
+if [[ -d distribution/conf/${TENANT}/prod ]]; then
+    # unpack the comaasqa package
+    tar xf distribution/target/distribution-${TENANT}-comaasqa.tar.gz -C distribution/target
+      
+    # Create a package for prod
+    PACKAGE_NAME="comaas-${TENANT}_prod-${TIMESTAMP}-${GIT_HASH}"
+    ARTIFACT_NAME="${PACKAGE_NAME}.tar.gz"
+    rm -rf distribution/target/distribution/conf
+    mkdir -p distribution/target/distribution/conf-${TENANT}
+    cp distribution/conf/${TENANT}/prod/* distribution/target/distribution/conf-${TENANT}
+    sed -i.bak "s~/conf ~/conf-${TENANT} ~" distribution/target/distribution/bin/comaas
+    chmod 775 distribution/target/distribution/log
+    mv distribution/target/distribution distribution/target/${PACKAGE_NAME}
+    tar czf ${BUILD_DIR}/${ARTIFACT_NAME} -C distribution/target/ ${PACKAGE_NAME}
+    rm -rf distribution/target/${PACKAGE_NAME}
+    echo "Created ${BUILD_DIR}/${ARTIFACT_NAME}"
+
+    # upload
+    ./bin/upload_ecg_repos.sh ${TENANT} ${BUILD_DIR}/${ARTIFACT_NAME} ${TIMESTAMP} prod
+fi
