df2297ae1f0bfc5ec65c190a07540d3a839e0fa3
Jeroen Smolenaers <jsmolenaers@ebay.com>
2017-01-11 14:59:51 +0100

    upload mp packages to repo
    Change-Id: I3329e65a0a0991d1cab053617818997ef419ed07
    Reviewed-on: https://gerrit.ecg.so/50619
    Tested-by: Gerrit-At-Gitmaster001
    Reviewed-by: Remmelt Pit <rpit@ebay.com>
    Tested-by: Comaas Jenkins <DL-eCG-comaas-dev@ebay.com>
    Reviewed-by: Geert Gerritsen <ggerritsen@ebay.com>
    Reviewed-by: Gerrit-At-Gitmaster001

--- bin/distribute.sh
+++ bin/distribute.sh
@@ -22,58 +22,52 @@ readonly BUILD_DIR=$5
 # Repackage into packages for each TENANT environment
 `dirname $0`/repackage.sh ${TENANT} ${GIT_HASH} ${BUILD_DIR}/${ARTIFACT_NAME} ${TIMESTAMP}
 
-case "${TENANT}" in
-  "mp")
-    readonly MP_PACKAGE_REGEX=".*/nl.marktplaats.mp-replyts2_comaas-([0-9a-zA-Z]+)-.*"
+if [[ "$TENANT" == "mp" ]] ; then
+  readonly MP_PACKAGE_REGEX=".*/nl.marktplaats.mp-replyts2_comaas-([0-9a-zA-Z]+)-.*"
 
-    # Upload or deploy (only the 3 most recent packages)
-    for PKG in $(ls -tdr ${BUILD_DIR}/nl.marktplaats.mp-replyts2* | tail -n3); do
+  # Upload or deploy (only the 3 most recent packages)
+  for PKG in $(ls -tdr ${BUILD_DIR}/nl.marktplaats.mp-replyts2* | tail -n3); do
 
-      if [[ ${PKG} =~ ${MP_PACKAGE_REGEX} ]]; then
-        DESTINATION="${BASH_REMATCH[1]}"
-      else
-        DESTINATION=""
-      fi
+    if [[ ${PKG} =~ ${MP_PACKAGE_REGEX} ]]; then
+      DESTINATION="${BASH_REMATCH[1]}"
+    else
+      DESTINATION=""
+    fi
 
-      if [[ "$DESTINATION" == "comaasqa" || "$DESTINATION" == "local" ]] ; then
-        continue
-      fi
+    if [[ "$DESTINATION" == "comaasqa" || "$DESTINATION" == "local" ]] ; then
+      continue
+    fi
 
-      # Deploy for mp demo, upload for all other environments/tenants
-      if [[ "$DESTINATION" == "demo" ]] ; then
-        # This requires the deploy.py script to be on the PATH
-        deploy.py --redeploy --config distribution/conf/mp/demo/deploy.conf --logdir . --component ${PKG} --ignore-lb
-      else
-        `dirname $0`/upload.sh ${TENANT} ${GIT_HASH} ${PKG} ${TIMESTAMP} ${DESTINATION}
-      fi
-    done
+    # Deploy for mp demo, upload for all other environments/tenants
+    if [[ "$DESTINATION" == "demo" ]] ; then
+      # This requires the deploy.py script to be on the PATH
+      deploy.py --redeploy --config distribution/conf/mp/demo/deploy.conf --logdir . --component ${PKG} --ignore-lb
+    else
+      `dirname $0`/upload.sh ${TENANT} ${GIT_HASH} ${PKG} ${TIMESTAMP} ${DESTINATION}
+    fi
+fi
 
-    ;;
-  *)
-    for PKG in $(ls ${BUILD_DIR}/comaas-${TENANT}*); do
-      PACKAGE_REGEX=".*/comaas-${TENANT}-(comaasqa|local).*"
-      [[ ${PKG} =~ ${PACKAGE_REGEX} ]] && continue
+for PKG in $(ls ${BUILD_DIR}/comaas-${TENANT}*); do
+  PACKAGE_REGEX=".*/comaas-${TENANT}-(comaasqa|local).*"
+  [[ ${PKG} =~ ${PACKAGE_REGEX} ]] && continue
 
-      # UPLOAD configuration
-      CONFIG_REGEX=".*/comaas-${TENANT}-configuration.*"
-      if [[ ${PKG} =~ ${CONFIG_REGEX} ]]; then
-        `dirname $0`/upload.sh ${TENANT} ${GIT_HASH} ${PKG} ${TIMESTAMP} sandbox
-#        `dirname $0`/upload.sh ${TENANT} ${GIT_HASH} ${PKG} ${TIMESTAMP} prod
-        continue
-      fi
+  # UPLOAD configuration
+  CONFIG_REGEX=".*/comaas-${TENANT}-configuration.*"
+  if [[ ${PKG} =~ ${CONFIG_REGEX} ]]; then
+    `dirname $0`/upload.sh ${TENANT} ${GIT_HASH} ${PKG} ${TIMESTAMP} sandbox
+    #  `dirname $0`/upload.sh ${TENANT} ${GIT_HASH} ${PKG} ${TIMESTAMP} prod
+    continue
+  fi
 
-      # UPLOAD SANDBOX PACKAGE
-      SANDBOX_REGEX=".*/comaas-${TENANT}_sandbox.*"
-      if [[ ${PKG} =~ ${SANDBOX_REGEX} ]]; then
-        `dirname $0`/upload.sh ${TENANT} ${GIT_HASH} ${PKG} ${TIMESTAMP} sandbox
-        continue
-      fi
+  # UPLOAD SANDBOX PACKAGE
+  SANDBOX_REGEX=".*/comaas-${TENANT}_sandbox.*"
+  if [[ ${PKG} =~ ${SANDBOX_REGEX} ]]; then
+    `dirname $0`/upload.sh ${TENANT} ${GIT_HASH} ${PKG} ${TIMESTAMP} sandbox
+    continue
+  fi
 
-      `dirname $0`/upload.sh ${TENANT} ${GIT_HASH} ${PKG} ${TIMESTAMP}
-      if [[ "${TENANT}" == "mde" ]]; then
-        `dirname $0`/upload.sh ${TENANT} ${GIT_HASH} ${PKG} ${TIMESTAMP} prod
-      fi
-    done
-
-    ;;
-esac
+  `dirname $0`/upload.sh ${TENANT} ${GIT_HASH} ${PKG} ${TIMESTAMP}
+  if [[ "${TENANT}" == "mde" ]]; then
+    `dirname $0`/upload.sh ${TENANT} ${GIT_HASH} ${PKG} ${TIMESTAMP} prod
+  fi
+done
