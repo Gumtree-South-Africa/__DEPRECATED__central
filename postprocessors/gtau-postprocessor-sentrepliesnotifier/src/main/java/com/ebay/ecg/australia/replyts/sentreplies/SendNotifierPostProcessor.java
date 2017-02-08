e2089f69c501ae0469248f78c87323bc87976309
hgiordano <hgiordano@gumtree.com>
2017-02-08 14:51:53 +1100

    GTAU-18624 Changed notifier so it accepts a new parameter that tells it to always notify clients.
    Change-Id: Id1653a248dfd2a1c5b3eae87ce434097e1ea036a
    Reviewed-on: https://gerrit.ecg.so/52067
    Tested-by: Gerrit-At-Gitmaster001
    Reviewed-by: Geert Gerritsen <ggerritsen@ebay.com>
    Tested-by: Comaas Jenkins <DL-eCG-comaas-dev@ebay.com>
    Reviewed-by: Remmelt Pit <rpit@ebay.com>
    Reviewed-by: Gerrit-At-Gitmaster001

--- postprocessors/gtau-postprocessor-sentrepliesnotifier/src/main/java/com/ebay/ecg/australia/replyts/sentreplies/SendNotifierPostProcessor.java
+++ postprocessors/gtau-postprocessor-sentrepliesnotifier/src/main/java/com/ebay/ecg/australia/replyts/sentreplies/SendNotifierPostProcessor.java
@@ -18,7 +18,11 @@ import java.net.URL;
  * @author mdarapour
  */
 public class SendNotifierPostProcessor implements PostProcessor {
+
+    public static final String ALWAYS_NOTIFY_FLAG = "Always-Notify-Flag";
+
     private static final String XML_BODY_CONTENT = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+
     private static final Logger LOGGER = LoggerFactory.getLogger(SendNotifierPostProcessor.class);
 
     private final SendNotifierConfig config;
@@ -28,23 +32,26 @@ public class SendNotifierPostProcessor implements PostProcessor {
         this.config = new SendNotifierConfig(endpointUrl);
     }
 
-    @Override
     public void postProcess(MessageProcessingContext context) {
         // Only if the message is from a buyer to a seller
         if (MessageDirection.BUYER_TO_SELLER == context.getMessageDirection()) {
             try {
                 Conversation conversation = context.getConversation();
-                // If this is the first message from the buyer to the seller
-                if (1 == conversation.getMessages().size()) {
+
+                if (this.isOkToNotify(conversation)) {
+
                     // check whether this is an XML formatted message used for posting to Autogate
                     final StringBuilder sbBodyText = new StringBuilder();
                     for (TypedContent<String> textPart : context.getOutgoingMail().getTextParts(false)) {
                         sbBodyText.append(textPart.getContent());
                     }
                     if (sbBodyText.toString().contains(XML_BODY_CONTENT)) {
-                        LOGGER.info("Message body contains XML content - ignoring this conversation id " + conversation.getId() + ", message id " + context.getMessageId() + " to prevent double counting of replies");
-                    } else {
-                        final String notifyUrl = config.getEndpointUrl() + "?adId=" + conversation.getAdId();
+                        LOGGER.info(
+                                "Message body contains XML content - ignoring this conversation id [{}], message id [{}] to prevent double counting of replies",
+                                conversation.getId(), context.getMessageId());
+                    }
+                    else {
+                        final String notifyUrl = this.buildNotifyUrl(conversation);
                         URL url = new URL(notifyUrl);
                         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                         try {
@@ -54,21 +61,58 @@ public class SendNotifierPostProcessor implements PostProcessor {
                             if (HttpURLConnection.HTTP_OK != responseCode) {
                                 LOGGER.warn(notifyUrl + ": " + responseCode);
                             }
-                        } catch (IOException e) {
+                        }
+                        catch (IOException e) {
                             LOGGER.error("Error notifying " + notifyUrl, e);
-                        } finally {
+                        }
+                        finally {
                             connection.disconnect();
                         }
                     }
                 }
-            } catch (Exception ex) {
+            }
+            catch (Exception ex) {
                 LOGGER.error(" Skipping it.", ex);
             }
         }
     }
 
-    @Override
+
+    private boolean isOkToNotify(Conversation conversation) {
+
+        // If this is the first message from the buyer to the seller
+        if (1 == conversation.getMessages().size()) {
+            return true;
+        }
+
+        // Check if notify override has been set.
+        // In this case always notify the client.
+        if (isNotifyOverride(conversation)) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    private String buildNotifyUrl(Conversation conversation) {
+        StringBuilder sb = new StringBuilder(config.getEndpointUrl());
+        sb.append("?adId=");
+        sb.append(conversation.getAdId());
+        // Let them know we were told to always notify in case they want to track it.
+        if (isNotifyOverride(conversation)) {
+            sb.append("&o=true");
+        }
+        return sb.toString();
+    }
+
+    private boolean isNotifyOverride(Conversation conversation) {
+        Object isNotifyOverrideFlag = conversation.getCustomValues().get(ALWAYS_NOTIFY_FLAG);
+        return (isNotifyOverrideFlag != null && Boolean.valueOf(isNotifyOverrideFlag.toString()));
+    }
+
     public int getOrder() {
         return 0;
     }
+
 }
