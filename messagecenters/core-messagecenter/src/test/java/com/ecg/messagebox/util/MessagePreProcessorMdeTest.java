b606a8ec4c3539dcc8115a5c2063a24cf25a7329
agorski <agorski@team.mobile.de>
2017-06-06 09:52:56 +0200

    FFI-2374 mde new patterns to match replies
    Change-Id: I07be0f51ac8a90d5115dc7c61e62ef2ecea4cbda
    Reviewed-on: https://gerrit.ecg.so/58226
    Tested-by: Gerrit-At-Gitmaster001
    Tested-by: Comaas Jenkins <DL-eCG-comaas-dev@ebay.com>
    Reviewed-by: Remmelt Pit <rpit@ebay.com>

--- messagecenters/core-messagecenter/src/test/java/com/ecg/messagebox/util/MessagePreProcessorMdeTest.java
+++ messagecenters/core-messagecenter/src/test/java/com/ecg/messagebox/util/MessagePreProcessorMdeTest.java
@@ -8,28 +8,32 @@ import java.util.List;
 
 public class MessagePreProcessorMdeTest extends MessagePreProcessorTest {
     private static final List<String> PATTERNS = Arrays.asList(
-      "\\n.*(a|b)-.*?@mail.mobile.de.*\\n",
-      "(Aan|To)\\s?:.*?@.*?",
-      "(Subject|Onderwerp|Betreff)\\s?:.*?",
-      "(Date|Datum)\\s?:.*?",
-      "\\n.*<[^<>\\s]+@gmail.[^<>\\s]+>.*\\n",
-      "\\b(?:<b>)?(From|To|Sender|Receiver|Van|Aan|Von|An|Gesendet) *: *(?:</b>)? *<a[^>]+href=\"mailto:[^\">]+@[^\">]+\"[^>]*>[^<]*</a",
-      "\\b(?:<b>)?(From|To|Sender|Receiver|Van|Aan|Von|An|Gesendet) *: *(?:</b>)? *(?:<[:a-z]+[^>]*>)?[^<>\\s]+@[^<>\\s]+(?:</[:a-z]+>)?",
-      "<span[^>]*>(From|To|Sender|Receiver|Van|Aan|Von|An|Gesendet) *: *</span *>[^<>]*(?:<[:a-z]+[^>]*>){0,2}[^<>\\s]+@[^<>\\s]+(?:</[:a-z]+>){0,2}",
-      "<b><span[^>]*>(From|To|Sender|Receiver|Van|Aan|Von|An|Gesendet) *: *</span *></b> *(?:<[:a-z]+[^>]*>)?[^<>\\s]+@[^<>\\s]+(?:</[:a-z]+>)?",
-      "<span[^>]*><b>(From|To|Sender|Receiver|Van|Aan|Von|An|Gesendet) *: *</b></span *> *(?:<[:a-z]+[^>]*>)?[^<>\\s]+@[^<>\\s]+(?:</[:a-z]+>)?",
-      "\\b(From|To|Sender|Receiver|Van|Aan|Von|An|Gesendet) *: *(<|&lt;)?[^<>\\s]+@[^<>\\s]+(>|&gt;)?",
-      "\\b(From|To|Sender|Receiver|Van|Aan|Von|An|Gesendet) *: *([^<>\\s]+ +){1,6}(<|&lt;)?[^<>\\s]+@[^<>\\s]+((<|&lt;)[^<>\\s]+@[^<>\\s]+(>|&gt;))?(>|&gt;)?",
-      "\\b(From|To|Sender|Receiver|Van|Aan|Von|An|Gesendet) *: *([^<>\\s]+ +){0,5}([^<>\\s]+)(<|&lt;)?[^<>\\s]+@[^<>\\s]+(>|&gt;)?",
-      "Op.{10,25}schrieb[^<]{5,60}<a[^>]+href=\"mailto:[^\">]+@[^\">]+\"[^>]*>[^<]*</a",
-      "Op.{10,25}schrieb[^<]{5,60}(<|&lt;)?\\s*[^<>\\s]+@[^<>\\s]+(>|&gt;)?",
-      "Am [0-9][0-9][0-9]?[0-9]?[./-].* schrieb.*",
-      "On [0-9][0-9][0-9]?[0-9]?[./-].* wrote.*",
-      "\\s+<b>*Gesendet*:*</b>",
-      "[0-9][0-9][0-9]?[0-9]?[./-].*buyer-.*@mail.mobile.de", // google client TOFU template
-      "[0-9][0-9][0-9]?[0-9]?[./-].*seller-.*@mail.mobile.de", // google client TOFU template
-      "[0-9][0-9][0-9]?[0-9]?[./-].*buyer-.*@kontakt.mobile.de", // google client TOFU template
-      "[0-9][0-9][0-9]?[0-9]?[./-].*seller-.*@kontakt.mobile.de" // google client TOFU template
+            //On Thu, Jun 1, 2017 at 1:32 PM
+            "On (Mo|Tu|We|Th|Fr|Sa|Su)[a-z]+, (Ja|Fe|Ma|Ap|Ma|Ju|Au|Se|Oc|No|De)[a-z]+ [0-3]?[0-9]\\, 2[0-9][0-9][0-9] at [0-2]?[0-9]:[0-5]?[0-9].*\\n",
+            //Gesendet: Donnerstag, 01. Juni 2017 um 13:56 Uhr"
+            "(Gesendet): (Mo|Di|Mi|Do|Fr|Sa|So)[a-z]+, [0-3]?[0-9]\\. (Ja|Fe|Mä|Ap|Ma|Ju|Au|Se|Ok|No|De)[a-z]+ 2[0-9][0-9][0-9] um [0-2]?[0-9]:[0-5]?[0-9].*\\n",
+            "(Aan|To)\\s?:.*?@.*?",
+            "(Subject|Onderwerp|Betreff)\\s?:.*?",
+            "(Date|Datum)\\s?:.*?",
+            "\\n.*<[^<>\\s]+@gmail.[^<>\\s]+>.*\\n",
+            "\\b(?:<b>)?(From|To|Sender|Receiver|Van|Aan|Von|An|Gesendet) *: *(?:</b>)? *<a[^>]+href=\"mailto:[^\">]+@[^\">]+\"[^>]*>[^<]*</a",
+            "\\b(?:<b>)?(From|To|Sender|Receiver|Van|Aan|Von|An|Gesendet) *: *(?:</b>)? *(?:<[:a-z]+[^>]*>)?[^<>\\s]+@[^<>\\s]+(?:</[:a-z]+>)?",
+            "<span[^>]*>(From|To|Sender|Receiver|Van|Aan|Von|An|Gesendet) *: *</span *>[^<>]*(?:<[:a-z]+[^>]*>){0,2}[^<>\\s]+@[^<>\\s]+(?:</[:a-z]+>){0,2}",
+            "<b><span[^>]*>(From|To|Sender|Receiver|Van|Aan|Von|An|Gesendet) *: *</span *></b> *(?:<[:a-z]+[^>]*>)?[^<>\\s]+@[^<>\\s]+(?:</[:a-z]+>)?",
+            "<span[^>]*><b>(From|To|Sender|Receiver|Van|Aan|Von|An|Gesendet) *: *</b></span *> *(?:<[:a-z]+[^>]*>)?[^<>\\s]+@[^<>\\s]+(?:</[:a-z]+>)?",
+            "\\b(From|To|Sender|Receiver|Van|Aan|Von|An|Gesendet) *: *(<|&lt;)?[^<>\\s]+@[^<>\\s]+(>|&gt;)?",
+            "\\b(From|To|Sender|Receiver|Van|Aan|Von|An|Gesendet) *: *([^<>\\s]+ +){1,6}(<|&lt;)?[^<>\\s]+@[^<>\\s]+((<|&lt;)[^<>\\s]+@[^<>\\s]+(>|&gt;))?(>|&gt;)?",
+            "\\b(From|To|Sender|Receiver|Van|Aan|Von|An|Gesendet) *: *([^<>\\s]+ +){0,5}([^<>\\s]+)(<|&lt;)?[^<>\\s]+@[^<>\\s]+(>|&gt;)?",
+            "Op.{10,25}schrieb[^<]{5,60}<a[^>]+href=\"mailto:[^\">]+@[^\">]+\"[^>]*>[^<]*</a",
+            "Op.{10,25}schrieb[^<]{5,60}(<|&lt;)?\\s*[^<>\\s]+@[^<>\\s]+(>|&gt;)?",
+            "Am [0-9][0-9][0-9]?[0-9]?[./-].* schrieb.*",
+            "On [0-9][0-9][0-9]?[0-9]?[./-].* wrote.*",
+            "\\s+<b>*Gesendet*:*</b>",
+            "\\n.*(s|b)-.*?@[a-z]+.mobile.de.*\\n",
+            "2[0-9][0-9][0-9][./-][0-9]?[0-9][./-][0-9]?[0-9] [0-2]?[0-9]:[0-5]?[0-9] .*buyer-.*@[a-z]+.mobile.de.*",
+            "2[0-9][0-9][0-9][./-][0-9]?[0-9][./-][0-9]?[0-9] [0-2]?[0-9]:[0-5]?[0-9] .*seller-.*@[a-z]+.mobile.de.*",
+            "[0-9][0-9][0-9]?[0-9]?[./-].*buyer-.*@[a-z]+.mobile.de.*\\n",
+            "[0-9][0-9][0-9]?[0-9]?[./-].*seller-.*@[a-z]+.mobile.de.*\\n"
     );
 
     @Test
@@ -69,6 +73,30 @@ public class MessagePreProcessorMdeTest extends MessagePreProcessorTest {
     public void realAnswer5Test() throws IOException {
         String msg = loadFileAsString("/com/ecg/messagecenter/util/mde/emailAnswer5.txt");
         String expected = loadFileAsString("/com/ecg/messagecenter/util/mde/emailAnswer5_cut.txt");
+
+        cutAndCompare(PATTERNS, msg, expected);
+    }
+
+    @Test
+    public void realAnswer6Test() throws IOException {
+        String msg = loadFileAsString("/com/ecg/messagecenter/util/mde/emailAnswer6.txt");
+        String expected = loadFileAsString("/com/ecg/messagecenter/util/mde/emailAnswer6_cut.txt");
+
+        cutAndCompare(PATTERNS, msg, expected);
+    }
+
+    @Test
+    public void realAnswer7Test() throws IOException {
+        String msg = loadFileAsString("/com/ecg/messagecenter/util/mde/emailAnswer7.txt");
+        String expected = loadFileAsString("/com/ecg/messagecenter/util/mde/emailAnswer7_cut.txt");
+
+        cutAndCompare(PATTERNS, msg, expected);
+    }
+
+    @Test
+    public void realAnswer8Test() throws IOException {
+        String msg = loadFileAsString("/com/ecg/messagecenter/util/mde/emailAnswer8.txt");
+        String expected = loadFileAsString("/com/ecg/messagecenter/util/mde/emailAnswer8_cut.txt");
 
         cutAndCompare(PATTERNS, msg, expected);
     }
