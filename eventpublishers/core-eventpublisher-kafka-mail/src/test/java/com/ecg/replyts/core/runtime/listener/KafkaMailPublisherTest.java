ff6794cfde8b37b93d6a2c9f5714c83e5db7f0dc
Aleksei Kobiakov <akobiakov@ebay.com>
2017-06-26 12:11:07 +0200

    Make KafkaMailPublisherTest deterministic
    Change-Id: Idf010876b0be9928ab97764193baf074902bdd4b
    Reviewed-on: https://gerrit.ecg.so/59371
    Tested-by: Gerrit-At-Gitmaster001
    Reviewed-by: Peter Troshin <p.troshin@ebay.com>
    Reviewed-by: Andrea Iacono <aiacono@ebay.com>
    Reviewed-by: Gerrit-At-Gitmaster001
    Tested-by: Comaas Jenkins <DL-eCG-comaas-dev@ebay.com>

--- eventpublishers/core-eventpublisher-kafka-mail/src/test/java/com/ecg/replyts/core/runtime/listener/KafkaMailPublisherTest.java
+++ eventpublishers/core-eventpublisher-kafka-mail/src/test/java/com/ecg/replyts/core/runtime/listener/KafkaMailPublisherTest.java
@@ -1,6 +1,7 @@
 package com.ecg.replyts.core.runtime.listener;
 
 import com.ecg.replyts.core.runtime.persistence.mail.StoredMail;
+import com.google.common.io.ByteStreams;
 import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
 import org.junit.Before;
@@ -11,10 +12,18 @@ import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 import static com.ecg.replyts.core.runtime.persistence.mail.StoredMail.extract;
 import static com.google.common.base.Optional.of;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -38,14 +47,34 @@ public class KafkaMailPublisherTest {
         byte[] data2 = new byte[]{0x02, 0x00, 0x00};
 
         mailPublisher.publishMail("key1", data1, of(data2));
-
         verify(producer).send(producedMessagesCaptor.capture());
         KeyedMessage<String, byte[]> m = producedMessagesCaptor.getValue();
         assertThat(m.topic(), is("test.core.mail"));
         assertThat(m.partitionKey(), is("key1"));
-        assertThat(m.message(), is(new StoredMail(data1, of(data2)).compress()));
+        assertEqualZipEntries(uncompress(m.message()), uncompress(new StoredMail(data1, of(data2)).compress()));
         StoredMail sm = extract(m.message());
         assertThat(sm.getInboundContents(), is(data1));
         assertThat(sm.getOutboundContents().orNull(), is(data2));
+    }
+
+    private static void assertEqualZipEntries(Map<String, byte[]> actual, Map<String, byte[]> expected) {
+        for (Map.Entry<String, byte[]> e : actual.entrySet()) {
+            String actualEntry = e.getKey();
+            byte[] payload = e.getValue();
+            assertTrue(expected.containsKey(actualEntry));
+            assertTrue(Arrays.equals(payload, expected.get(actualEntry)));
+        }
+    }
+
+    private static Map<String, byte[]> uncompress(byte[] zippedStuff) {
+        try (ZipInputStream zis = new ZipInputStream(new ByteArrayInputStream(zippedStuff))) {
+            Map<String, byte[]> entries = new HashMap<>();
+            for (ZipEntry e; (e = zis.getNextEntry()) != null;) {
+                entries.put(e.getName(), ByteStreams.toByteArray(zis));
+            }
+            return entries;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
