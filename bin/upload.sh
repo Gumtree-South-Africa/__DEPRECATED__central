7c1ce96c686bc365822bf74cf4d02826bad1542a
Remmelt Pit <rpit@ebay.com>
2017-07-11 12:42:06 +0200

    Fix GTUK pipeline, cleanup /bin
    Change-Id: If3a230a94bb292cf04050177b57a9aaa7a8f0aed
    Reviewed-on: https://gerrit.ecg.so/60125
    Tested-by: Gerrit-At-Gitmaster001
    Reviewed-by: Remmelt Pit <rpit@ebay.com>

--- bin/upload.sh
+++ bin/upload.sh
@@ -24,7 +24,6 @@ function parseArgs() {
 
 # map to lookup the upload hosts for a tenant
 declare -A HOSTS=(
-  ["ebayk"]="https://comaas-uploader:ohy9Te#hah9U@kautodeploy.corp.mobile.de/storage/belen-productive-deployment-releases/com/ecg/ebayk/comaas/"
   ["kjca"]='http://comaas:bMv!Yne7Apj3F4pW@nexus.kjdev.ca/content/repositories/comaas/ecg/comaas/versions/'
   ["gtau"]=""
 )
@@ -65,33 +64,11 @@ if [ ${TENANT} == "gtuk" ]; then
         echo "Package to upload not found: $DEB_PACK"
         exit 1
     fi
-    ./bin/upload_ecg_repos.sh ${TENANT} ${DEB_PACK} ${TIMESTAMP} legacy
-    exit
-fi
-if [ ${TENANT} == "mp" ]; then
-    echo "Uploading the legacy MP package to the iCAS deployer"
-	readonly PREFIX="comaas"
-	readonly REMOTE_DIR=/opt/tarballs/ecg-comaas
-	readonly RELEASE_FOLDER="${PREFIX}-${TIMESTAMP}"
-	readonly KEEPVERSIONS=20
-	readonly HOST_VALUE="builder@deploy001.esh.ops.prod.icas.ecg.so"
-
-
-	echo "Syncing $PACKAGE to ${HOST_VALUE} ..."
-
-	set -o xtrace
-	ssh ${HOST_VALUE} "mkdir -p ${REMOTE_DIR}/${RELEASE_FOLDER}"
-	rsync -avv --no-times --no-o --no-p --size-only \
-		${PACKAGE} \
-		${HOST_VALUE}:${REMOTE_DIR}/${RELEASE_FOLDER}/$(basename $PACKAGE)
-
-#	echo "Cleaning up old versions on ${HOST_VALUE} ..."
-#	ssh ${HOST_VALUE} "ls -td ${REMOTE_DIR}/${PREFIX}-* | sed -ne'1,${KEEPVERSIONS}!p' | xargs -r rm -rv"
-	set +o xtrace
+    ./bin/upload_ecg_swift.sh ${TENANT} ${DEB_PACK} ${TIMESTAMP} legacy
     exit
 fi
 
-if [[ "$TENANT" == mde ]] ; then
+if [[ "$TENANT" == "mde" ]] || [[ "$TENANT" == "mp" ]] || [[ "$TENANT" == "ebayk" ]]; then
     echo "Uploading not supported for $TENANT, because it's already live in the cloud"
     exit
 fi
