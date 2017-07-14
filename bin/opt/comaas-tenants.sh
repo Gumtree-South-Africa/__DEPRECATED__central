c4654eb099ea91ab6048ff84c336c8081a9516ca
Remmelt Pit <rpit@ebay.com>
2017-07-14 14:12:00 +0200

    Added sandbox to the where-does-tenant-run script
    Change-Id: Ifec64fe44c270af4c2ec5ee1eea3eb38ed4f6f2c
    Reviewed-on: https://gerrit.ecg.so/60394
    Tested-by: Gerrit-At-Gitmaster001
    Reviewed-by: Remmelt Pit <rpit@ebay.com>

--- bin/opt/comaas-tenants.sh
+++ bin/opt/comaas-tenants.sh
@@ -6,12 +6,16 @@ get_region () {
     echo $ip | egrep -q '^10\.32\.56\.' && echo -n dus1
 }
 
-for tenant in uk mo mp ca au ek; do
-    ip=$(dig +short ${tenant}.prod.comaas.ecg.so)
-    region=$(get_region ${ip})
-    if [ -z ${region} ]; then
-        region="n/a"
-    fi
-    echo "${tenant}: ${ip} (region: ${region})"
+for env in prod sandbox; do
+    echo Environment: ${env}
+    for tenant in uk mo mp ca au ek; do
+        ip=$(dig +short ${tenant}.${env}.comaas.ecg.so)
+        region=$(get_region ${ip})
+        if [ -z ${region} ]; then
+            region="n/a"
+        fi
+        printf "${tenant}:\t${ip}\t(region: ${region})\n"
+    done
+    echo
 done
 
