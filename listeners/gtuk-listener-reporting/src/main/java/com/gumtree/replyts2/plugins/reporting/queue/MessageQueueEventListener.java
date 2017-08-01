aaf565f2672a331e0c7d7f49c2ac51f61e59b15f
Remmelt Pit <rpit@ebay.com>
2017-08-01 10:45:44 +0200

    GTUK removed unnecessary stacktrace for missing categoryId
    Change-Id: Ia7ee45b6f7cbf9220ec1abf370f045d2904cc5d1
    Reviewed-on: https://gerrit.ecg.so/61273
    Tested-by: Gerrit-At-Gitmaster001
    Reviewed-by: Remmelt Pit <rpit@ebay.com>

--- listeners/gtuk-listener-reporting/src/main/java/com/gumtree/replyts2/plugins/reporting/queue/MessageQueueEventListener.java
+++ listeners/gtuk-listener-reporting/src/main/java/com/gumtree/replyts2/plugins/reporting/queue/MessageQueueEventListener.java
@@ -9,7 +9,6 @@ import com.ecg.replyts.core.runtime.listener.MessageProcessedListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 import java.util.Optional;
 
 /**
@@ -17,11 +16,9 @@ import java.util.Optional;
  * added to for messages which get 'HELD' and removed from when they are 'timed out', approved or declined by an agent
  */
 public class MessageQueueEventListener implements MessageProcessedListener {
-
     private MessageQueueManager messageQueueManager;
     private Clock clock;
     private static final Logger LOG = LoggerFactory.getLogger(MessageQueueEventListener.class);
-
 
     public MessageQueueEventListener(MessageQueueManager messageQueueManager, Clock clock) {
         this.messageQueueManager = messageQueueManager;
@@ -30,25 +27,21 @@ public class MessageQueueEventListener implements MessageProcessedListener {
 
     @Override
     public void messageProcessed(Conversation conversation, Message message) {
-
         if (!getCategoryId(conversation).isPresent()) {
-             return;
+            return;
         }
 
         if (held(message)) {
             LOG.debug("Held message to be inserted into db queue: %s", message.getId());
             messageQueueManager.enQueueMessage(message.getId(), getCategoryId(conversation).get(), clock.now());
             LOG.debug("Held message queued: %s", message.getId());
-
         } else if (message.getHumanResultState().isAcceptableOutcome()) {
             LOG.debug("Held message actioned and will be removed from db queue: %s", message.getId());
             boolean success = messageQueueManager.deQueueMessage(message.getId());
             if (success) {
                 LOG.debug("Held message removed from db queue: %s", message.getId());
             }
-
         }
-
     }
 
     private Optional<Long> getCategoryId(Conversation conversation) {
@@ -56,7 +49,7 @@ public class MessageQueueEventListener implements MessageProcessedListener {
         try {
             return Optional.of(Long.parseLong(categoryId));
         } catch (NumberFormatException nfe) {
-            LOG.warn("No category Id found when attempting to add to message queue!", nfe);
+            LOG.warn("No category Id found when attempting to add to message queue! {}", nfe.getMessage());
             return Optional.empty();
         }
     }
