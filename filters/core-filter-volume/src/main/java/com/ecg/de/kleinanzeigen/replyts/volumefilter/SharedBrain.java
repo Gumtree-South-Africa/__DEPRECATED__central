aeb59a138fa775aad7e7f78f6b3b52bb84fa58c5
Petr Bouda <pbouda@ebay.com>
2017-08-15 14:01:35 +0200

    Change to use Reliable Topic in Hazelcast.
    Change-Id: Ie9fba0c5213c9c0b751a6f4913b807b5e22103e0
    Reviewed-on: https://gerrit.ecg.so/62000
    Tested-by: Gerrit-At-Gitmaster001
    Reviewed-by: Aleksei Kobiakov <akobiakov@ebay.com>
    Reviewed-by: Oleksandr Shmatko <oshmatko@ebay.com>
    Reviewed-by: Gerrit-At-Gitmaster001
    Tested-by: Comaas Jenkins <DL-eCG-comaas-dev@ebay.com>

--- filters/core-filter-volume/src/main/java/com/ecg/de/kleinanzeigen/replyts/volumefilter/SharedBrain.java
+++ filters/core-filter-volume/src/main/java/com/ecg/de/kleinanzeigen/replyts/volumefilter/SharedBrain.java
@@ -4,12 +4,14 @@ import com.hazelcast.core.*;
 
 public class SharedBrain {
 
+    private static final String VOLUMEFILTER_SENDER_ADDRESS_EXCHANGE = "volumefilter_sender_address_exchange";
+
     private final ITopic<String> communicationBus;
     private final EventStreamProcessor processor;
 
     SharedBrain(HazelcastInstance hazelcastInstance, EventStreamProcessor processor) {
         this.processor = processor;
-        this.communicationBus = hazelcastInstance.getTopic("volumefilter_sender_address_exchange");
+        this.communicationBus = hazelcastInstance.getReliableTopic(VOLUMEFILTER_SENDER_ADDRESS_EXCHANGE);
         this.communicationBus.addMessageListener(message -> {
             Member publishingMember = message.getPublishingMember();
             if (publishingMember != null && !publishingMember.localMember()) {
