8081cd8795e0554ce7d7c4851340cd690a3209a4
Peter Troshin <ptroshin@ebay.com>
2017-10-03 09:43:02 +0200

    ES cleanup job is not DC specific e.g. it has to run in active DC at least, better in both.
    Change-Id: Id2d03797cb505422e7d9386312e8eb80bc0d1d80
    Reviewed-on: https://gerrit.ecg.so/64592
    Tested-by: Gerrit-At-Gitmaster001
    Tested-by: Comaas Jenkins <DL-eCG-comaas-dev@ebay.com>
    Reviewed-by: Andrea Iacono <aiacono@ebay.com>
    Reviewed-by: Oleksandr Shmatko <oshmatko@ebay.com>
    Reviewed-by: Gerrit-At-Gitmaster001

--- core-runtime/src/main/java/com/ecg/replyts/app/cronjobs/CleanupSearchCronJob.java
+++ core-runtime/src/main/java/com/ecg/replyts/app/cronjobs/CleanupSearchCronJob.java
@@ -8,14 +8,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import static com.ecg.replyts.core.runtime.cron.CronExpressionBuilder.everyNMinutes;
 import static org.joda.time.DateTime.now;
 
 @Component
-@ConditionalOnExpression("#{'${replyts2.cronjob.cleanupSearch.enabled:true}' == 'true' || '${replyts2.cronjob.cleanupSearch.enabled:true}' == '${region}'}")
+@ConditionalOnProperty(name = "replyts2.cronjob.cleanupSearch.enabled", havingValue = "true")
 public class CleanupSearchCronJob implements CronJobExecutor {
     private static final Logger LOG = LoggerFactory.getLogger(CleanupSearchCronJob.class);
 
