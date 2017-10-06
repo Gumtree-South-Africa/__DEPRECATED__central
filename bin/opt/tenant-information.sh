f6c2aac875b07d884fd3f1d77ed9b17bda5ef41f
Remmelt Pit <rpit@ebay.com>
2017-10-06 14:19:32 +0200

    More tenant information
    Change-Id: I76e0bac22ef1484c0d08b6d3e69ec2adb4b76c25
    Reviewed-on: https://gerrit.ecg.so/64836
    Tested-by: Gerrit-At-Gitmaster001
    Reviewed-by: Remmelt Pit <rpit@ebay.com>

--- bin/opt/tenant-information.sh
+++ bin/opt/tenant-information.sh
@@ -1,38 +1,61 @@
 #!/usr/bin/env bash
 
+set -o nounset
+
+# tenant env
 get_region () {
-    ip=$1
-    echo $ip | egrep -q '^10\.32\.24\.' && echo -n ams1
-    echo $ip | egrep -q '^10\.32\.56\.' && echo -n dus1
+    local ip=$(dig +short ${1}.${2}.comaas.ecg.so)
+    echo ${ip} | egrep -q '^10\.32\.24\.' && echo -n ams1
+    echo ${ip} | egrep -q '^10\.32\.56\.' && echo -n dus1
 }
 
+# tenant region dc
 get_info () {
-    echo $(curl -m 1 -s $1.$2.comaas.ecg.so/health)
+    echo $(curl -m 1 -s $3.$1.$2.comaas.ecg.so/health)
+}
+
+# tenant active_dc ams1_version ams1_mode dus1_version dus1_mode
+print_row () {
+#echo $@
+    printf '%-8s | %6s | %-8s | %-9s | %-8s | %-9s\n' $1 $2 $3 $4 $5 $6
+}
+
+# key json
+from_json () {
+    what=${1}
+    shift
+    v=$(echo ${@} | jq -r .${what})
+    if [ -z ${v} ]; then
+        v="x"
+    fi
+    echo ${v}
 }
 
 for env in prod sandbox; do
-    echo Environment: ${env}
-    printf "tenant\tversion\tregion\tip\t\tmode\n"
-    for tenant in uk mo mp ca au ek; do
-        ip=$(dig +short ${tenant}.${env}.comaas.ecg.so)
-        region=$(get_region ${ip})
-        if [ -z ${region} ]; then
-            region="n/a"
+    print_row ${env} active ams1 ams1 dus1 dus1
+    print_row tenant dc version mode version mode
+    echo "---------------------------------------------------------------"
+
+    for tenant in uk mo mp ca au ek it; do
+        active_dc=$(get_region ${tenant} ${env})
+        if [ -z ${active_dc} ]; then
+            active_dc="-"
         fi
 
-        info=$(get_info ${tenant} ${env})
+        info_ams1=$(get_info ${tenant} ${env} ams1)
+        v_ams1=$(from_json version ${info_ams1})
+        p_ams1=$(from_json conversationRepositorySource ${info_ams1})
 
-        version=$(echo $info | jq -r .version)
-        if [ -z ${version} ]; then
-            version="n/a"
+        if [ ${env} == "sandbox" ]; then
+            v_dus1="n/a"
+            p_dus1="n/a"
+        else
+            info_dus1=$(get_info ${tenant} ${env} dus1)
+            v_dus1=$(from_json version ${info_dus1})
+            p_dus1=$(from_json conversationRepositorySource ${info_dus1})
         fi
 
-        persistence_strategy=$(echo $info | jq -r .conversationRepositorySource)
-        if [ -z ${persistence_strategy} ]; then
-            persistence_strategy="n/a"
-        fi
-        printf "${tenant}\t${version}\t${region}\t${ip}\t${persistence_strategy}\n"
+        print_row ${tenant} ${active_dc} ${v_ams1} ${p_ams1} ${v_dus1} ${p_dus1}
     done
     echo
 done
-
