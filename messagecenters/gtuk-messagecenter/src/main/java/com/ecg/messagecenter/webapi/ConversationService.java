2d98f2bf235b6f27401cae25dd990a3631928868
Petr Bouda <pbouda@ebay.com>
2017-10-27 14:57:38 +0200

    GTUK Digging up business logic from UK Conversation Controller to Service class. - Simply copy-pasted from controller to service to be able to implement diffing between MessageCenter and MessageBox
    Change-Id: I1df454a576caf35ba8d3f7980aaa1586eb1f4856
    Reviewed-on: https://gerrit.ecg.so/65872
    Tested-by: Gerrit-At-Gitmaster001
    Tested-by: Comaas Jenkins <DL-eCG-comaas-dev@ebay.com>
    Reviewed-by: Andrea Iacono <aiacono@ebay.com>
    Reviewed-by: Oleksandr Shmatko <oshmatko@ebay.com>
    Reviewed-by: Gerrit-At-Gitmaster001

---
+++ messagecenters/gtuk-messagecenter/src/main/java/com/ecg/messagecenter/webapi/ConversationService.java
@@ -0,0 +1,211 @@
+package com.ecg.messagecenter.webapi;
+
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Timer;
+import com.ecg.messagecenter.persistence.ConversationThread;
+import com.ecg.messagecenter.persistence.simple.PostBox;
+import com.ecg.messagecenter.persistence.simple.PostBoxId;
+import com.ecg.messagecenter.persistence.simple.SimplePostBoxRepository;
+import com.ecg.messagecenter.webapi.responses.PostBoxSingleConversationThreadResponse;
+import com.ecg.replyts.app.ConversationEventListeners;
+import com.ecg.replyts.core.api.model.MailCloakingService;
+import com.ecg.replyts.core.api.model.conversation.Conversation;
+import com.ecg.replyts.core.api.model.conversation.ConversationRole;
+import com.ecg.replyts.core.api.model.conversation.MutableConversation;
+import com.ecg.replyts.core.api.model.conversation.command.AddCustomValueCommand;
+import com.ecg.replyts.core.api.webapi.model.ConversationRts;
+import com.ecg.replyts.core.runtime.TimingReports;
+import com.ecg.replyts.core.runtime.indexer.conversation.SearchIndexer;
+import com.ecg.replyts.core.runtime.persistence.conversation.DefaultMutableConversation;
+import com.ecg.replyts.core.runtime.persistence.conversation.MutableConversationRepository;
+import com.ecg.replyts.core.webapi.screeningv2.converter.DomainObjectConverter;
+import com.gumtree.gumshield.api.client.GumshieldApi;
+import com.gumtree.gumshield.api.domain.user_message_report.ApiFlaggedConversation;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+
+@Component
+public class ConversationService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConversationService.class);
+
+    private static final Timer API_POSTBOX_CONVERSATION_BY_ID = TimingReports.newTimer("webapi-postbox-conversation-by-id");
+    private static final Timer API_POSTBOX_CONVERSATION_BY_ID_RIAK = TimingReports.newTimer("webapi-postbox-conversation-by-id-riak");
+    private static final Timer API_POSTBOX_CONVERSATION_BY_ID_MSG_PROCESSING = TimingReports.newTimer("webapi-postbox-conversation-by-id-message-processing");
+    private static final Histogram API_NUM_REQUESTED_NUM_MESSAGES_OF_CONVERSATION = TimingReports.newHistogram("webapi-postbox-num-messages-of-conversation");
+
+    private static final Object SUCCESS = new Object();
+
+    private final SimplePostBoxRepository postBoxRepository;
+    private final MutableConversationRepository conversationRepository;
+    private final SearchIndexer searchIndexer;
+    private final DomainObjectConverter converter;
+    private final GumshieldApi gumshieldApi;
+    private final ConversationEventListeners conversationEventListeners;
+
+    @Autowired
+    public ConversationService(
+            SimplePostBoxRepository postBoxRepository,
+            MutableConversationRepository conversationRepository,
+            SearchIndexer searchIndexer,
+            MailCloakingService mailCloakingService,
+            GumshieldApi gumshieldApi,
+            ConversationEventListeners conversationEventListeners) {
+
+        this.conversationRepository = conversationRepository;
+        this.postBoxRepository = postBoxRepository;
+        this.searchIndexer = searchIndexer;
+        this.converter = new DomainObjectConverter(conversationRepository, mailCloakingService);
+        this.gumshieldApi = gumshieldApi;
+        this.conversationEventListeners = conversationEventListeners;
+    }
+
+    Optional<PostBoxSingleConversationThreadResponse> getConversation(String email, String conversationId) {
+        try (Timer.Context ignore = API_POSTBOX_CONVERSATION_BY_ID.time()) {
+            PostBox postBox = postBoxRepository.byId(PostBoxId.fromEmail(email));
+            Optional<ConversationThread> conversationThreadRequested = postBox.lookupConversation(conversationId);
+            if (!conversationThreadRequested.isPresent()) {
+                return Optional.empty();
+            }
+
+            return lookupConversation(postBox.getNewRepliesCounter().getValue(), email, conversationId);
+        }
+    }
+
+    Optional<PostBoxSingleConversationThreadResponse> readConversation(String email, String conversationId) {
+        try (Timer.Context ignore = API_POSTBOX_CONVERSATION_BY_ID.time()) {
+            PostBox postBox = postBoxRepository.byId(PostBoxId.fromEmail(email));
+            Optional<ConversationThread> conversationThreadRequested = postBox.lookupConversation(conversationId);
+            if (!conversationThreadRequested.isPresent()) {
+                return Optional.empty();
+            }
+
+            if (conversationThreadRequested.get().isContainsUnreadMessages()) {
+                int unreadMessages = postBoxRepository.unreadCountInConversation(PostBoxId.fromEmail(postBox.getEmail()), conversationId);
+                postBox.decrementNewReplies(unreadMessages);
+                postBoxRepository.markConversationAsRead(postBox, conversationThreadRequested.get());
+            }
+
+            return lookupConversation(postBox.getNewRepliesCounter().getValue(), email, conversationId);
+        }
+    }
+
+    Optional<Object> reportConversation(String email, String conversationId) {
+        MutableConversation conversation = conversationRepository.getById(conversationId);
+        ConversationRole buyerOrSeller = emailBelongsToBuyerOrSeller(conversation, email);
+        if (buyerOrSeller == null) {
+            return Optional.empty();
+        }
+
+        DateTime now = DateTime.now();
+        markConversation(conversation, new FlaggedCustomValue(buyerOrSeller, ConversationCustomValue.AT_POSTFIX, now.toString()));
+        markConversation(conversation, new FlaggedCustomValue(buyerOrSeller, ConversationCustomValue.DATE_POSTFIX, now.toLocalDate().toString()));
+
+        ((DefaultMutableConversation) conversation).commit(conversationRepository, conversationEventListeners);
+        searchIndexer.updateSearchSync(Collections.singletonList(conversation));
+
+        PostBox postBox = postBoxRepository.byId(PostBoxId.fromEmail(email));
+        markConversationAsRead(conversationId, postBox);
+
+        ConversationRts conversationRts = converter.convertConversation(conversation);
+
+        ApiFlaggedConversation flaggedConversation = new ApiFlaggedConversation();
+        flaggedConversation.setAdvertId(Long.valueOf(conversationRts.getAdId()));
+        flaggedConversation.setConversationId(conversationRts.getId());
+        flaggedConversation.setReportedByEmail(email);
+        flaggedConversation.setReportedDate(DateTime.parse(getFlaggedTime(conversationRts, email)));
+        flaggedConversation.setReportedForEmail(getOtherEmail(conversationRts, email));
+        gumshieldApi.conversationApi().flagConversation(flaggedConversation);
+        return Optional.of(SUCCESS);
+    }
+
+    Optional<ConversationRts> markDeletedConversation(String email, String conversationId) {
+        MutableConversation conversation = conversationRepository.getById(conversationId);
+        ConversationRole buyerOrSeller = emailBelongsToBuyerOrSeller(conversation, email);
+        if (buyerOrSeller == null) {
+            return Optional.empty();
+        }
+
+        DateTime now = DateTime.now();
+        markConversation(conversation, new DeletedCustomValue(buyerOrSeller, ConversationCustomValue.AT_POSTFIX, now.toString()));
+        markConversation(conversation, new DeletedCustomValue(buyerOrSeller, ConversationCustomValue.DATE_POSTFIX, now.toLocalDate().toString()));
+
+        ((DefaultMutableConversation) conversation).commit(conversationRepository, conversationEventListeners);
+        searchIndexer.updateSearchSync(Collections.singletonList(conversation));
+
+        PostBox postBox = postBoxRepository.byId(PostBoxId.fromEmail(email));
+        markConversationAsRead(conversationId, postBox);
+
+        return Optional.ofNullable(converter.convertConversation(conversation));
+    }
+
+    private Optional<PostBoxSingleConversationThreadResponse> lookupConversation(long numUnread, String email, String conversationId) {
+        Conversation conversation;
+        try (Timer.Context ignored = API_POSTBOX_CONVERSATION_BY_ID_RIAK.time()) {
+            conversation = conversationRepository.getById(conversationId);
+        }
+
+        // can only happen if both buckets diverge
+        if (conversation == null) {
+            LOGGER.warn("Inconsistency: Conversation id #{} exists in 'postbox' bucket but not inside 'conversations' bucket", conversationId);
+            return Optional.empty();
+        }
+
+        Optional<PostBoxSingleConversationThreadResponse> created;
+        try (Timer.Context ignored = API_POSTBOX_CONVERSATION_BY_ID_MSG_PROCESSING.time()) {
+            created = PostBoxSingleConversationThreadResponse.create(numUnread, email, conversation);
+        }
+
+        if (created.isPresent()) {
+            API_NUM_REQUESTED_NUM_MESSAGES_OF_CONVERSATION.update(created.get().getMessages().size());
+            return created;
+        } else {
+            LOGGER.info("Conversation id #{} is empty but was accessed from list-view, should normally not be reachable by UI", conversationId);
+            return Optional.empty();
+        }
+    }
+
+    private void markConversation(MutableConversation conversation, ConversationCustomValue customValue) {
+        conversation.applyCommand(new AddCustomValueCommand(conversation.getId(), customValue.keyName(), customValue.value()));
+    }
+
+    private ConversationRole emailBelongsToBuyerOrSeller(Conversation conversation, String email) {
+        if (conversation != null) {
+            if (conversation.getBuyerId().equalsIgnoreCase(email)) {
+                return ConversationRole.Buyer;
+            } else if (conversation.getSellerId().equalsIgnoreCase(email)) {
+                return ConversationRole.Seller;
+            }
+        }
+        return null;
+    }
+
+    private void markConversationAsRead(String conversationId, PostBox<ConversationThread> postBox) {
+        for (ConversationThread item : postBox.getConversationThreads()) {
+            if (item.getConversationId().equals(conversationId) && item.isContainsUnreadMessages()) {
+                postBoxRepository.markConversationAsRead(postBox, item);
+                break;
+            }
+        }
+    }
+
+    private String getOtherEmail(ConversationRts conversation, String email) {
+        return conversation.getBuyer().equalsIgnoreCase(email)
+                ? conversation.getSeller()
+                : conversation.getBuyer();
+    }
+
+    private String getFlaggedTime(ConversationRts conversation, String email) {
+        Map<String, String> conversationHeaders = conversation.getConversationHeaders();
+        return conversation.getBuyer().equalsIgnoreCase(email)
+                ? conversationHeaders.get("flagged-buyer-at")
+                : conversationHeaders.get("flagged-seller-at");
+    }
+}
--- messagecenters/gtuk-messagecenter/src/main/java/com/ecg/messagecenter/webapi/ConversationThreadController.java
+++ messagecenters/gtuk-messagecenter/src/main/java/com/ecg/messagecenter/webapi/ConversationThreadController.java
@@ -1,35 +1,13 @@
 package com.ecg.messagecenter.webapi;
 
-import com.codahale.metrics.Histogram;
-import com.codahale.metrics.Timer;
 import com.ecg.messagebox.controllers.TopLevelExceptionHandler;
-import com.ecg.messagecenter.persistence.ConversationThread;
-import com.ecg.messagecenter.persistence.simple.PostBox;
-import com.ecg.messagecenter.persistence.simple.PostBoxId;
-import com.ecg.messagecenter.persistence.simple.SimplePostBoxRepository;
 import com.ecg.messagecenter.webapi.requests.MessageCenterDeleteConversationCommand;
 import com.ecg.messagecenter.webapi.requests.MessageCenterGetPostBoxConversationCommand;
 import com.ecg.messagecenter.webapi.requests.MessageCenterReportConversationCommand;
 import com.ecg.messagecenter.webapi.responses.PostBoxSingleConversationThreadResponse;
-import com.ecg.replyts.app.ConversationEventListeners;
-import com.ecg.replyts.core.api.model.MailCloakingService;
-import com.ecg.replyts.core.api.model.conversation.Conversation;
-import com.ecg.replyts.core.api.model.conversation.ConversationRole;
-import com.ecg.replyts.core.api.model.conversation.MutableConversation;
-import com.ecg.replyts.core.api.model.conversation.command.AddCustomValueCommand;
 import com.ecg.replyts.core.api.webapi.envelope.RequestState;
 import com.ecg.replyts.core.api.webapi.envelope.ResponseObject;
 import com.ecg.replyts.core.api.webapi.model.ConversationRts;
-import com.ecg.replyts.core.runtime.TimingReports;
-import com.ecg.replyts.core.runtime.indexer.conversation.SearchIndexer;
-import com.ecg.replyts.core.runtime.persistence.conversation.DefaultMutableConversation;
-import com.ecg.replyts.core.runtime.persistence.conversation.MutableConversationRepository;
-import com.ecg.replyts.core.webapi.screeningv2.converter.DomainObjectConverter;
-import com.gumtree.gumshield.api.client.GumshieldApi;
-import com.gumtree.gumshield.api.domain.user_message_report.ApiFlaggedConversation;
-import org.joda.time.DateTime;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
 import org.springframework.http.HttpStatus;
@@ -40,39 +18,16 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.*;
 
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class ConversationThreadController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ConversationThreadController.class);
 
-    private static final Timer API_POSTBOX_CONVERSATION_BY_ID = TimingReports.newTimer("webapi-postbox-conversation-by-id");
-    private static final Timer API_POSTBOX_CONVERSATION_BY_ID_RIAK = TimingReports.newTimer("webapi-postbox-conversation-by-id-riak");
-    private static final Timer API_POSTBOX_CONVERSATION_BY_ID_MSG_PROCESSING = TimingReports.newTimer("webapi-postbox-conversation-by-id-message-processing");
-    private static final Histogram API_NUM_REQUESTED_NUM_MESSAGES_OF_CONVERSATION = TimingReports.newHistogram("webapi-postbox-num-messages-of-conversation");
-
-    private final SimplePostBoxRepository postBoxRepository;
-    private final MutableConversationRepository conversationRepository;
-    private final SearchIndexer searchIndexer;
-    private final DomainObjectConverter converter;
-    private final GumshieldApi gumshieldApi;
-    private final ConversationEventListeners conversationEventListeners;
+    private final ConversationService conversationService;
 
     @Autowired
-    public ConversationThreadController(
-            MutableConversationRepository conversationRepository,
-            SimplePostBoxRepository postBoxRepository,
-            SearchIndexer searchIndexer,
-            MailCloakingService mailCloakingService,
-            GumshieldApi gumshieldApi,
-            ConversationEventListeners conversationEventListeners) {
-        this.conversationRepository = conversationRepository;
-        this.postBoxRepository = postBoxRepository;
-        this.searchIndexer = searchIndexer;
-        this.converter = new DomainObjectConverter(conversationRepository, mailCloakingService);
-        this.gumshieldApi = gumshieldApi;
-        this.conversationEventListeners = conversationEventListeners;
+    public ConversationThreadController(ConversationService conversationService) {
+        this.conversationService = conversationService;
     }
 
     @InitBinder
@@ -85,176 +40,57 @@ public class ConversationThreadController {
         TopLevelExceptionHandler.handle(ex, response);
     }
 
-    @PostMapping(MessageCenterReportConversationCommand.MAPPING)
-    ResponseEntity<ResponseObject<?>> reportConversation(
-            @PathVariable("email") String email,
-            @PathVariable("conversationId") String conversationId) {
-
-        MutableConversation conversation = conversationRepository.getById(conversationId);
-        ConversationRole buyerOrSeller = emailBelongsToBuyerOrSeller(conversation, email);
-        if (buyerOrSeller == null) {
-            return entityNotFound();
-        }
-
-        DateTime now = DateTime.now();
-        markConversation(conversation, new FlaggedCustomValue(buyerOrSeller, ConversationCustomValue.AT_POSTFIX, now.toString()));
-        markConversation(conversation, new FlaggedCustomValue(buyerOrSeller, ConversationCustomValue.DATE_POSTFIX, now.toLocalDate().toString()));
-
-        ((DefaultMutableConversation) conversation).commit(conversationRepository, conversationEventListeners);
-        searchIndexer.updateSearchSync(Collections.singletonList(conversation));
-
-        PostBox postBox = postBoxRepository.byId(PostBoxId.fromEmail(email));
-        markConversationAsRead(conversationId, postBox);
-
-        ConversationRts conversationRts = converter.convertConversation(conversation);
-
-        ApiFlaggedConversation flaggedConversation = new ApiFlaggedConversation();
-        flaggedConversation.setAdvertId(Long.valueOf(conversationRts.getAdId()));
-        flaggedConversation.setConversationId(conversationRts.getId());
-        flaggedConversation.setReportedByEmail(email);
-        flaggedConversation.setReportedDate(DateTime.parse(getFlaggedTime(conversationRts, email)));
-        flaggedConversation.setReportedForEmail(getOtherEmail(conversationRts, email));
-        gumshieldApi.conversationApi().flagConversation(flaggedConversation);
-        return ResponseEntity.ok(new ResponseObject<>());
-    }
-
-    private String getOtherEmail(ConversationRts conversation, String email) {
-        return conversation.getBuyer().equalsIgnoreCase(email)
-                ? conversation.getSeller()
-                : conversation.getBuyer();
-    }
-
-    private String getFlaggedTime(ConversationRts conversation, String email) {
-        Map<String, String> conversationHeaders = conversation.getConversationHeaders();
-        return conversation.getBuyer().equalsIgnoreCase(email)
-                ? conversationHeaders.get("flagged-buyer-at")
-                : conversationHeaders.get("flagged-seller-at");
-    }
-
-    @DeleteMapping(MessageCenterDeleteConversationCommand.MAPPING)
-    ResponseEntity<ResponseObject<?>> deleteConversation(
-            @PathVariable("email") String email,
-            @PathVariable("conversationId") String conversationId) {
-
-        MutableConversation conversation = conversationRepository.getById(conversationId);
-        ConversationRole buyerOrSeller = emailBelongsToBuyerOrSeller(conversation, email);
-        if (buyerOrSeller == null) {
-            return entityNotFound();
-        }
-
-        DateTime now = DateTime.now();
-        markConversation(conversation, new DeletedCustomValue(buyerOrSeller, ConversationCustomValue.AT_POSTFIX, now.toString()));
-        markConversation(conversation, new DeletedCustomValue(buyerOrSeller, ConversationCustomValue.DATE_POSTFIX, now.toLocalDate().toString()));
-
-        ((DefaultMutableConversation) conversation).commit(conversationRepository, conversationEventListeners);
-        searchIndexer.updateSearchSync(Collections.singletonList(conversation));
-
-        PostBox postBox = postBoxRepository.byId(PostBoxId.fromEmail(email));
-        markConversationAsRead(conversationId, postBox);
-
-        ConversationRts conversationRts = converter.convertConversation(conversation);
-        return ResponseEntity.ok(ResponseObject.of(conversationRts));
-    }
-
-
-    private void markConversation(MutableConversation conversation, ConversationCustomValue customValue) {
-        conversation.applyCommand(new AddCustomValueCommand(conversation.getId(), customValue.keyName(), customValue.value()));
-    }
-
-    private ConversationRole emailBelongsToBuyerOrSeller(Conversation conversation, String email) {
-        if (conversation != null) {
-            if (conversation.getBuyerId().equalsIgnoreCase(email)) {
-                return ConversationRole.Buyer;
-            } else if (conversation.getSellerId().equalsIgnoreCase(email)) {
-                return ConversationRole.Seller;
-            }
-        }
-        return null;
-    }
-
     /*
      * newCounterMode is never used, default is always used = true
      */
     @GetMapping(MessageCenterGetPostBoxConversationCommand.MAPPING)
-    ResponseEntity<ResponseObject<?>> getConversation(
+    public ResponseEntity<ResponseObject<PostBoxSingleConversationThreadResponse>> getConversation(
             @PathVariable("email") String email,
             @PathVariable("conversationId") String conversationId) {
 
-        try (Timer.Context ignore = API_POSTBOX_CONVERSATION_BY_ID.time()) {
-            PostBox postBox = postBoxRepository.byId(PostBoxId.fromEmail(email));
-
-            Optional<ConversationThread> conversationThreadRequested = postBox.lookupConversation(conversationId);
-            if (!conversationThreadRequested.isPresent()) {
-                return entityNotFound();
-            }
-
-            return lookupConversation(postBox.getNewRepliesCounter().getValue(), email, conversationId);
-        }
+        return conversationService.getConversation(email, conversationId)
+                .map(ResponseObject::of)
+                .map(ResponseEntity::ok)
+                .orElseGet(this::entityNotFound);
     }
 
     /*
      * newCounterMode is never used, default is always used = true
      */
     @PutMapping(MessageCenterGetPostBoxConversationCommand.MAPPING)
-    ResponseEntity<ResponseObject<?>> readConversation(
+    public ResponseEntity<ResponseObject<PostBoxSingleConversationThreadResponse>> readConversation(
             @PathVariable("email") String email,
             @PathVariable("conversationId") String conversationId) {
 
-        try (Timer.Context ignore = API_POSTBOX_CONVERSATION_BY_ID.time()) {
-            PostBox postBox = postBoxRepository.byId(PostBoxId.fromEmail(email));
-
-            Optional<ConversationThread> conversationThreadRequested = postBox.lookupConversation(conversationId);
-            if (!conversationThreadRequested.isPresent()) {
-                return entityNotFound();
-            }
-
-            if (conversationThreadRequested.get().isContainsUnreadMessages()) {
-                int unreadMessages = postBoxRepository.unreadCountInConversation(PostBoxId.fromEmail(postBox.getEmail()), conversationId);
-                postBox.decrementNewReplies(unreadMessages);
-                postBoxRepository.markConversationAsRead(postBox, conversationThreadRequested.get());
-            }
-
-            return lookupConversation(postBox.getNewRepliesCounter().getValue(), email, conversationId);
-        }
+        return conversationService.readConversation(email, conversationId)
+                .map(ResponseObject::of)
+                .map(ResponseEntity::ok)
+                .orElseGet(this::entityNotFound);
     }
 
-    private ResponseEntity<ResponseObject<?>> lookupConversation(long numUnread, String email, String conversationId) {
-        Conversation conversation;
-        try (Timer.Context ignored = API_POSTBOX_CONVERSATION_BY_ID_RIAK.time()) {
-            conversation = conversationRepository.getById(conversationId);
-        }
+    @PostMapping(MessageCenterReportConversationCommand.MAPPING)
+    public ResponseEntity<ResponseObject> reportConversation(
+            @PathVariable("email") String email,
+            @PathVariable("conversationId") String conversationId) {
 
-        // can only happen if both buckets diverge
-        if (conversation == null) {
-            LOGGER.warn("Inconsistency: Conversation id #{} exists in 'postbox' bucket but not inside 'conversations' bucket", conversationId);
-            return entityNotFound();
-        }
-
-        Optional<PostBoxSingleConversationThreadResponse> created;
-        try (Timer.Context ignored = API_POSTBOX_CONVERSATION_BY_ID_MSG_PROCESSING.time()) {
-            created = PostBoxSingleConversationThreadResponse.create(numUnread, email, conversation);
-        }
-
-        if (created.isPresent()) {
-            API_NUM_REQUESTED_NUM_MESSAGES_OF_CONVERSATION.update(created.get().getMessages().size());
-            return ResponseEntity.ok(ResponseObject.of(created.get()));
-        } else {
-            LOGGER.info("Conversation id #{} is empty but was accessed from list-view, should normally not be reachable by UI", conversationId);
-            return entityNotFound();
-        }
-
+        return conversationService.reportConversation(email, conversationId)
+                .map(obj -> new ResponseObject())
+                .map(ResponseEntity::ok)
+                .orElseGet(this::entityNotFound);
     }
 
-    private void markConversationAsRead(String conversationId, PostBox<ConversationThread> postBox) {
-        for (ConversationThread item : postBox.getConversationThreads()) {
-            if (item.getConversationId().equals(conversationId) && item.isContainsUnreadMessages()) {
-                postBoxRepository.markConversationAsRead(postBox, item);
-                break;
-            }
-        }
+    @DeleteMapping(MessageCenterDeleteConversationCommand.MAPPING)
+    public ResponseEntity<ResponseObject<ConversationRts>> deleteConversation(
+            @PathVariable("email") String email,
+            @PathVariable("conversationId") String conversationId) {
+
+        return conversationService.markDeletedConversation(email, conversationId)
+                .map(ResponseObject::of)
+                .map(ResponseEntity::ok)
+                .orElseGet(this::entityNotFound);
     }
 
-    private ResponseEntity<ResponseObject<?>> entityNotFound() {
+    private ResponseEntity entityNotFound() {
         return new ResponseEntity<>(ResponseObject.of(RequestState.ENTITY_NOT_FOUND), HttpStatus.NOT_FOUND);
     }
 }
