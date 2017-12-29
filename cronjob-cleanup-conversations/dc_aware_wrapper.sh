c7fa6e157a2989edec09e939a2049a17f6bcdd73
Aleksei Kobiakov <akobiakov@ebay.com>
2017-12-29 14:06:43 +0100

    DC awareness wrapper

---
+++ cronjob-cleanup-conversations/dc_aware_wrapper.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#$NOMAD_RERGION is made available through nomad and is usable as an environment variable. This will contain the lowercase ams1 or dus1 region
+
+ACTIVEDC=`dig +short ek.lp.comaas.cloud TXT | sed 's/^....//' | sed 's/.$//' | awk '{print tolower($0)}' `
+
+if [ "$ACTIVEDC" != "ams1" ] && [ "$ACTIVEDC" != "dus1" ]
+then
+  echo "I don't know the returned dc, please check if lookup fails"
+  exit 1
+fi
+
+if [ "$NOMAD_REGION" == "$ACTIVEDC" ]
+then
+  echo "This is the activedc, not running the cronjob"
+  exit 0
+else
+  echo "This is not the active dc, so running the command"
+  java -jar cronjobs-cleanup-conversations-1-SNAPSHOT-jar-with-dependencies.jar
+fi
