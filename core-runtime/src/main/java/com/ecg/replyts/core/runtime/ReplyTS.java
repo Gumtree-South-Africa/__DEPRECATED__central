26bc640954da6e4848f67f53bce94287d3d768af
Van Veghel, Jasper(jvanveghel) <jvanveghel@ebay.com>
2018-01-22 17:06:44 +0100

    Added back the ReplyTS class - for now - so that comaas.sh needn't be changed (#202)
    We can s/\.runtime\.ReplyTS/\.Application/ in ecg-comaas-salt's comaas.sh
    as soon as we don't have to be able to start or roll back to a version
    that doesn't yet have Application#main.

---
+++ core-runtime/src/main/java/com/ecg/replyts/core/runtime/ReplyTS.java
@@ -0,0 +1,10 @@
+package com.ecg.replyts.core.runtime;
+
+import com.ecg.replyts.core.Application;
+
+@Deprecated
+public class ReplyTS {
+    public static void main(String[] args) {
+        Application.main(args);
+    }
+}
