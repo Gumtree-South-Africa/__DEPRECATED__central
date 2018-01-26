86a96003018f2c50a3c6045a6394eea09857c320
Claudio Tagliola <ctagliola@ebay.com>
2018-01-26 12:54:34 +0100

    Added setDefaultUncaughtExceptionHandler to handle cronjob stacktraces (#220)

--- core-runtime/src/main/java/com/ecg/replyts/core/Application.java
+++ core-runtime/src/main/java/com/ecg/replyts/core/Application.java
@@ -12,12 +12,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static java.lang.String.format;
@@ -30,6 +37,8 @@ public class Application {
     private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
     static {
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> LOG.error("Uncaught exception in thread {}", thread, throwable));
+
         LoggingService.bootstrap();
     }
 
