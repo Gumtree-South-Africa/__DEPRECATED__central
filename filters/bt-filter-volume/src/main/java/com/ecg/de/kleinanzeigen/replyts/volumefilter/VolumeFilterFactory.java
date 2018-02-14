18b62bf204a49eff9ab77171c4041cd872115a62
Van Veghel, Jasper(jvanveghel) <jvanveghel@ebay.com>
2018-02-13 18:53:19 -0800

    Makes it so that SharedBrain is only ever instantiated once (#281)

--- filters/bt-filter-volume/src/main/java/com/ecg/de/kleinanzeigen/replyts/volumefilter/VolumeFilterFactory.java
+++ filters/bt-filter-volume/src/main/java/com/ecg/de/kleinanzeigen/replyts/volumefilter/VolumeFilterFactory.java
@@ -2,22 +2,19 @@ package com.ecg.de.kleinanzeigen.replyts.volumefilter;
 
 import com.ecg.replyts.core.api.pluginconfiguration.filter.Filter;
 import com.ecg.replyts.core.api.pluginconfiguration.filter.FilterFactory;
-import com.ecg.replyts.core.runtime.ComaasPlugin;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.hazelcast.core.HazelcastInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@ComaasPlugin
 @Component
 public class VolumeFilterFactory implements FilterFactory {
     @Autowired
-    private HazelcastInstance hazelcastInstance;
+    private SharedBrain sharedBrain;
 
     public Filter createPlugin(String filterName, JsonNode jsonNode) {
         ConfigurationParser config = new ConfigurationParser(jsonNode);
 
-        return new VolumeFilter(filterName, hazelcastInstance,
+        return new VolumeFilter(filterName, sharedBrain,
           config.getQuotas(),
           config.isIgnoreFollowUps(),
           config.getExceptCategoriesList(),
