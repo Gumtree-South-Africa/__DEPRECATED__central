931821a91e88bd2556b99ecbddfc94cd14ec4b62
Petr Bouda <pbouda@ebay.com>
2018-02-15 09:28:40 +0100

    Update of synchronization mechanism for UK (similar to EBAYK)

--- messagecenters/gtuk-messagecenter/src/main/java/com/ecg/messagecenter/diff/UserIdNotFoundException.java
+++ messagecenters/gtuk-messagecenter/src/main/java/com/ecg/messagecenter/diff/UserIdNotFoundException.java
@@ -2,13 +2,18 @@ package com.ecg.messagecenter.diff;
 
 class UserIdNotFoundException extends RuntimeException {
 
-    final static String UNKNOWN = "<unknown>";
+    private final boolean loggable;
 
-    UserIdNotFoundException(String email) {
-        this(email, UNKNOWN, UNKNOWN);
+    UserIdNotFoundException(String message) {
+        this(message, true);
     }
 
-    UserIdNotFoundException(String email, String role, String conversationId) {
-        super(String.format("UserId was not found for email: %s, role: %s, conversation: %s", email, role, conversationId));
+    UserIdNotFoundException(String message, boolean loggable) {
+        super(message);
+        this.loggable = loggable;
+    }
+
+    public boolean isLoggable() {
+        return loggable;
     }
 }
