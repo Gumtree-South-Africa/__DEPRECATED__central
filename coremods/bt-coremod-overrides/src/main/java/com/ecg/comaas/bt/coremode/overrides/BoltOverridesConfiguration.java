619ce47b05770e82872e92f9c3664a12ab1e5f8c
Andrea Iacono <aiacono@ebay.com>
2018-04-16 11:54:12 +0200

    added @ComaasPlugin to Bolt overrider for HtmlRemover. (#570)

--- coremods/bt-coremod-overrides/src/main/java/com/ecg/comaas/bt/coremode/overrides/BoltOverridesConfiguration.java
+++ coremods/bt-coremod-overrides/src/main/java/com/ecg/comaas/bt/coremode/overrides/BoltOverridesConfiguration.java
@@ -1,6 +1,7 @@
 package com.ecg.comaas.bt.coremode.overrides;
 
 import com.ecg.replyts.app.postprocessorchain.postprocessors.Anonymizer;
+import com.ecg.replyts.core.api.pluginconfiguration.ComaasPlugin;
 import com.ecg.replyts.core.runtime.mailcloaking.MultiTenantMailCloakingService;
 import com.ecg.replyts.core.runtime.mailparser.HtmlRemover;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Primary;
 
 // Will get picked up by the Application @ComponentScan (com.ecg.replyts.app.*)
 
+@ComaasPlugin
 @Configuration
 public class BoltOverridesConfiguration {
     static {
