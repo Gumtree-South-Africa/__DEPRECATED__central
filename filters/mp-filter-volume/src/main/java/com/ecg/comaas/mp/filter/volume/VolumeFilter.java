b09ecf9d10fc9609bb34721be3779971093db0d6
Alexey Kobyakov <akobiakov@ebay.com>
2018-06-15 18:09:24 +0200

    COMAAS-1145 - fix MP VolumeFilter vulnerability (#782)
    * COMAAS-1145 - fix MP VolumeFilter vulnerability
    
    * COMAAS-1145 - fix MP VolumeFilter vulnerability

--- filters/mp-filter-volume/src/main/java/com/ecg/comaas/mp/filter/volume/VolumeFilter.java
+++ filters/mp-filter-volume/src/main/java/com/ecg/comaas/mp/filter/volume/VolumeFilter.java
@@ -9,10 +9,14 @@ import com.ecg.replyts.core.api.model.mail.Mail;
 import com.ecg.replyts.core.api.pluginconfiguration.filter.Filter;
 import com.ecg.replyts.core.api.pluginconfiguration.filter.FilterFeedback;
 import com.ecg.replyts.core.api.processing.MessageProcessingContext;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Iterables;
 
 import java.util.Collections;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class VolumeFilter implements Filter {
 
@@ -37,23 +41,29 @@ public class VolumeFilter implements Filter {
         Conversation conv = context.getConversation();
         Message message = context.getMessage();
 
-        if (isFirstMailInConversation(conv, message) && !isBid(conv)) {
-            recordMessage(message, conv);
+        if (isBid(conv)) {
+            return Collections.emptyList();
+        }
 
-            String userId = extractFrom(message, conv);
-            for (VolumeRule rule : sortedRules) {
-                int sentActually = volumeFilterEventRepository.count(userId, (int) rule.getTimeUnit().toSeconds(rule.getTimeSpan()));
-                if (rule.getMaxCount() < sentActually) {
-                    Optional<Mail> mail = context.getMail();
-                    if (mail.isPresent()) {
-                        String uiHint = toUiHint(rule, mail.get().getFrom());
-                        String description = toDescription(rule, mail.get().getFrom());
-                        return Collections.singletonList(new FilterFeedback(uiHint, description, rule.getScore(), FilterResultState.OK));
-                    } else {
-                        String uiHint = toUiHint(rule, userId);
-                        String description = toDescription(rule, userId);
-                        return Collections.singletonList(new FilterFeedback(uiHint, description, rule.getScore(), FilterResultState.OK));
-                    }
+        if (!isFirstMailInConversation(conv, message) && !wasPreviousMessageBySameUserDroppedOrHeld(conv, extractFrom(message, conv))) {
+            return Collections.emptyList();
+        }
+
+        recordMessage(message, conv);
+
+        String userId = extractFrom(message, conv);
+        for (VolumeRule rule : sortedRules) {
+            int sentActually = volumeFilterEventRepository.count(userId, (int) rule.getTimeUnit().toSeconds(rule.getTimeSpan()));
+            if (sentActually > rule.getMaxCount()) {
+                Optional<Mail> mail = context.getMail();
+                if (mail.isPresent()) {
+                    String uiHint = toUiHint(rule, mail.get().getFrom());
+                    String description = toDescription(rule, mail.get().getFrom());
+                    return Collections.singletonList(new FilterFeedback(uiHint, description, rule.getScore(), FilterResultState.OK));
+                } else {
+                    String uiHint = toUiHint(rule, userId);
+                    String description = toDescription(rule, userId);
+                    return Collections.singletonList(new FilterFeedback(uiHint, description, rule.getScore(), FilterResultState.OK));
                 }
             }
         }
@@ -72,8 +82,23 @@ public class VolumeFilter implements Filter {
         return BID_FLOW_TYPE.equals(conv.getCustomValues().get("flowtype"));
     }
 
-    private boolean isFirstMailInConversation(Conversation conversation, Message message) {
+    private static boolean isFirstMailInConversation(Conversation conversation, Message message) {
         return conversation.getMessages().get(0).getId().equals(message.getId());
+    }
+
+    @VisibleForTesting
+    static boolean wasPreviousMessageBySameUserDroppedOrHeld(Conversation conv, String userId) {
+        if (conv.getMessages() == null || conv.getMessages().size() == 0) {
+            return false;
+        }
+        List<Message> messagesByUser = conv.getMessages().stream()
+                .filter(m -> userId.equals(extractFrom(m, conv)))
+                .collect(Collectors.toList());
+        if (messagesByUser.size() == 0) {
+            return false;
+        }
+        Message lastMessage = Iterables.getLast(messagesByUser);
+        return EnumSet.of(FilterResultState.DROPPED, FilterResultState.HELD).contains(lastMessage.getFilterResultState());
     }
 
     private void recordMessage(Message message, Conversation conversation) {
@@ -87,7 +112,7 @@ public class VolumeFilter implements Filter {
         volumeFilterEventRepository.record(userId, longestTimeSpan.intValue());
     }
 
-    private String extractFrom(Message message, Conversation conv) {
+    private static String extractFrom(Message message, Conversation conv) {
         return conv.getUserId(message.getMessageDirection().getFromRole());
     }
 }
