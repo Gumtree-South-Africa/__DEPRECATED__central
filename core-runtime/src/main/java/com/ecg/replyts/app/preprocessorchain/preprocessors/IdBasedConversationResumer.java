b119cc3ed31e34d0549db8f23aaa47d515dde78a
roshetty <roshetty@ebay.com>
2018-06-21 12:45:48 +0200

    [COMAAS-1084] Adding log messages to track id headers

--- core-runtime/src/main/java/com/ecg/replyts/app/preprocessorchain/preprocessors/IdBasedConversationResumer.java
+++ core-runtime/src/main/java/com/ecg/replyts/app/preprocessorchain/preprocessors/IdBasedConversationResumer.java
@@ -8,6 +8,8 @@ import com.ecg.replyts.core.api.persistence.ConversationIndexKey;
 import com.ecg.replyts.core.api.persistence.ConversationRepository;
 import com.ecg.replyts.core.api.processing.MessageProcessingContext;
 import com.ecg.replyts.core.runtime.identifier.UserIdentifierService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
@@ -18,6 +20,7 @@ import java.util.Optional;
 @Component
 @ConditionalOnProperty(value = "conversation.resume.strategy", havingValue = "id")
 public class IdBasedConversationResumer extends ConversationResumer {
+    private static final Logger LOG = LoggerFactory.getLogger(IdBasedConversationResumer.class);
 
     @Autowired
     private UserIdentifierService userIdentifierService;
@@ -29,6 +32,7 @@ public class IdBasedConversationResumer extends ConversationResumer {
         Map<String, String> headers = mail.getCustomHeaders();
         String buyerAddress = Optional.ofNullable(mail.getReplyTo()).orElse(mail.getFrom());
         String sellerAddress = mail.getDeliveredTo();
+
         String buyerId = userIdentifierService.getBuyerUserId(headers).orElse(buyerAddress);
         String sellerId = userIdentifierService.getSellerUserId(headers).orElse(sellerAddress);
 
@@ -38,10 +42,18 @@ public class IdBasedConversationResumer extends ConversationResumer {
         ConversationIndexKey indexKeyBuyerToSellerEmail = new ConversationIndexKey(buyerAddress, sellerAddress, mail.getAdId());
         ConversationIndexKey indexKeySellerToBuyerEmail = new ConversationIndexKey(sellerAddress, buyerAddress, mail.getAdId());
 
+        if (!userIdentifierService.getBuyerUserId(headers).isPresent()) {
+            LOG.debug("ID Headers are absent for Buyer. Header name {}", userIdentifierService.getBuyerUserIdName());
+        }
+        if (!userIdentifierService.getSellerUserId(headers).isPresent()) {
+            LOG.debug("ID Headers are absent for Seller. Header name {}", userIdentifierService.getSellerUserIdName());
+        }
+
         return tryResume(repository, context, MessageDirection.BUYER_TO_SELLER, indexKeyBuyerToSeller)
                 || tryResume(repository, context, MessageDirection.SELLER_TO_BUYER, indexKeySellerToBuyer)
 
-//                when tenant switched from email based conversation id to userid based conversation id, we need to check for existing conversation which were email based before the switch
+                //when tenant switched from email based conversation id to userid based conversation id, we need to check for existing conversation which were email based before the switch
+                //TODO:[COMAAS-1163] Remove below 2 checks once all the emails are sent with id headers. We can check for above log messages
                 || tryResume(repository, context, MessageDirection.BUYER_TO_SELLER, indexKeyBuyerToSellerEmail)
                 || tryResume(repository, context, MessageDirection.SELLER_TO_BUYER, indexKeySellerToBuyerEmail)
                 ;
