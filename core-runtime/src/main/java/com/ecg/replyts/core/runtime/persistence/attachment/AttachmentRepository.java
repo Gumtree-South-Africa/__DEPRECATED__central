9ed1d3656c48cc06f07b96a6f4124500d0f195b9
Aleksei Kobiakov <akobiakov@ebay.com>
2018-06-28 11:40:06 +0200

    Cleanup

--- core-runtime/src/main/java/com/ecg/replyts/core/runtime/persistence/attachment/AttachmentRepository.java
+++ core-runtime/src/main/java/com/ecg/replyts/core/runtime/persistence/attachment/AttachmentRepository.java
@@ -3,12 +3,8 @@ package com.ecg.replyts.core.runtime.persistence.attachment;
 import com.codahale.metrics.Counter;
 import com.codahale.metrics.Histogram;
 import com.codahale.metrics.Timer;
-import com.ecg.replyts.app.Mails;
-import com.ecg.replyts.core.api.model.mail.Mail;
 import com.ecg.replyts.core.api.processing.Attachment;
-import com.ecg.replyts.core.api.processing.MessageProcessingContext;
 import com.ecg.replyts.core.runtime.TimingReports;
-import com.ecg.replyts.core.runtime.mailparser.ParsingException;
 import com.ecg.replyts.core.runtime.persistence.kafka.KafkaSinkService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.Collection;
-import java.util.List;
 
 // Interface to Swift repository for reading attachments and Kafka repository for writing them
 public class AttachmentRepository {
@@ -24,7 +19,6 @@ public class AttachmentRepository {
     private static final Logger LOG = LoggerFactory.getLogger(AttachmentRepository.class);
 
     private final Counter MAIL_WITH_ATTACHMENTS_COUNTER = TimingReports.newCounter("attachments.mail-with-attachment-counter");
-    private final Counter MAIL_COUNTER = TimingReports.newCounter("attachments.mail-counter");
     private final Histogram ATT_PER_MAIL_HISTOGRAM = TimingReports.newHistogram("attachments.number-of-attachments-per-message");
     private final Histogram MAIL_SIZE_HISTOGRAM = TimingReports.newHistogram("attachments.total-size");
     private final Histogram ATTACHMENT_SIZE_HISTOGRAM = TimingReports.newHistogram("attachments.attachment.size");
@@ -37,33 +31,6 @@ public class AttachmentRepository {
 
     @Autowired
     private SwiftAttachmentRepository swiftAttachmentRepository;
-
-    public Mail hasAttachments(String messageId, byte[] mailData) {
-
-        Mail parsedMail;
-        List<String> attNames;
-
-        if (mailData.length <= 0) {
-            LOG.info("No data in message '{}'", messageId);
-            return null;
-        }
-
-        try {
-            parsedMail = Mails.readMail(mailData);
-            attNames = parsedMail.getAttachmentNames();
-        } catch (ParsingException e) {
-            LOG.error("Could not parse message id {}, fail to save its attachments", messageId);
-            return null;
-        }
-
-        MAIL_COUNTER.inc();
-        if (attNames.isEmpty()) {
-            LOG.trace("Message id {} has no attachments ", messageId);
-            return null;
-        }
-
-        return parsedMail;
-    }
 
     public String getCompositeKey(String messageId, String attachmentName) {
         String container = swiftAttachmentRepository.getContainer(messageId);
