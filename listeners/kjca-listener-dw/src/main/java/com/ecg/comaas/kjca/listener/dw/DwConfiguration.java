6c498f9d3eeec33bc9477c9d2cfece5a95b7942c
Petr Bouda <pbouda@ebay.com>
2018-11-20 15:41:10 +0100

    Fixed leaking AMQ Component to all tenants

--- listeners/kjca-listener-dw/src/main/java/com/ecg/comaas/kjca/listener/dw/DwConfiguration.java
+++ listeners/kjca-listener-dw/src/main/java/com/ecg/comaas/kjca/listener/dw/DwConfiguration.java
@@ -19,7 +19,6 @@ import static com.ecg.replyts.core.api.model.Tenants.TENANT_MVCA;
 @ComaasPlugin
 @Profile({TENANT_KJCA, TENANT_MVCA})
 @Configuration
-@Import(ActiveMQReporter.class)
 public class DwConfiguration {
 
     private static final String AMQ_CONN_ARGS =
