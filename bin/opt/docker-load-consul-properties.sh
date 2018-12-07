f510829a7279b75cb772d2e582001fd811b57937
Eric Feliksik <efeliksik@ebay.com>
2018-12-07 16:08:21 +0100

    rename config to properties

--- bin/opt/docker-load-consul-properties.sh
+++ bin/opt/docker-load-consul-properties.sh
@@ -11,15 +11,15 @@ die(){
 [ -z "$TENANT" ] && die "Need a tenant name"
 
 CONFDIR=${PWD}/distribution/conf
-TENANT_CONFIG=$CONFDIR/$TENANT/docker.properties
+TENANT_PROPERTIES=$CONFDIR/$TENANT/docker.properties
 
 [ -d "$CONFDIR" ] || die "Confdir not found: $CONFDIR . Are you in project-Central root?"
-[ -f "$TENANT_CONFIG" ] || die "No config file for tenant $TENANT: $TENANT_CONFIG "
+[ -f "$TENANT_PROPERTIES" ] || die "No properties config file for tenant $TENANT: $TENANT_PROPERTIES"
 
-echo "Loading to consul: tenant config file $TENANT_CONFIG"
+echo "Loading to consul: tenant properties from file $TENANT_PROPERTIES"
 
 docker run --network comaasdocker_default --rm \
-	--volume ${PWD}/distribution/conf/$TENANT/docker.properties:/props.properties \
+	--volume $TENANT_PROPERTIES:/props.properties \
         dock.es.ecg.tools/comaas/properties-to-consul:0.0.7 -consul http://comaasdocker_consul_1:8500 \
 	-tenant $TENANT
 
