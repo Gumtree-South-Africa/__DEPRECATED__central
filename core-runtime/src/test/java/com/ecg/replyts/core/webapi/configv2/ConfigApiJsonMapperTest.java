825e6f39da4f9ee3872d89cf2b612398d359dbbd
Eric Feliksik <efeliksik@ebay.com>
2019-01-08 13:23:37 +0100

    remove mockito

--- core-runtime/src/test/java/com/ecg/replyts/core/webapi/configv2/ConfigApiJsonMapperTest.java
+++ core-runtime/src/test/java/com/ecg/replyts/core/webapi/configv2/ConfigApiJsonMapperTest.java
@@ -8,19 +8,13 @@ import com.ecg.replyts.core.api.util.JsonObjects;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.commons.lang3.NotImplementedException;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Spy;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
 public class ConfigApiJsonMapperTest {
 
     @Test
