86ac1eca5519e6663b619cb6a73b18a1bb52b742
Eric Feliksik <efeliksik@ebay.com>
2019-01-09 13:14:45 +0100

    clean up the ConfigV2Controller by factoring out the Json mapper (#1011)
    * clean up the ConfigV2Controller by factoring out the Json mapper
    * remove mockito
    * remove dead code

---
+++ core-runtime/src/main/java/com/ecg/replyts/core/webapi/configv2/ConfigApiJsonMapper.java
@@ -0,0 +1,93 @@
+package com.ecg.replyts.core.webapi.configv2;
+
+import com.ecg.replyts.core.api.configadmin.ConfigurationId;
+import com.ecg.replyts.core.api.configadmin.PluginConfiguration;
+import com.ecg.replyts.core.api.pluginconfiguration.PluginState;
+import com.ecg.replyts.core.api.util.JsonObjects;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.String.format;
+
+public class ConfigApiJsonMapper {
+
+    static class Model {
+
+        /**
+         * configurations as a json object
+         */
+        public static ObjectNode toJson(List<PluginConfiguration> configList) {
+            ArrayNode arrayNode = JsonObjects.newJsonArray();
+            for (PluginConfiguration pluginConfiguration : configList) {
+                JsonObjects.Builder config = JsonObjects.builder()
+                        .attr("pluginFactory", pluginConfiguration.getId().getPluginFactory())
+                        .attr("instanceId", pluginConfiguration.getId().getInstanceId())
+                        .attr("priority", pluginConfiguration.getPriority())
+                        .attr("state", pluginConfiguration.getState().name())
+                        .attr("version", pluginConfiguration.getVersion())
+                        .attr("configuration", pluginConfiguration.getConfiguration());
+                arrayNode.add(config.build());
+            }
+            return JsonObjects.builder().attr("configs", arrayNode).build();
+        }
+
+    }
+
+    static class Json {
+
+        public static List<PluginConfiguration> toPluginConfigurationList(ArrayNode body) throws Exception {
+            List<PluginConfiguration> pluginConfigurations = new ArrayList<>();
+
+            for (JsonNode node : body) {
+                assertArrayElement(node, "pluginFactory", TextNode.class);
+                assertArrayElement(node, "instanceId", TextNode.class);
+
+                String pluginFactory = node.get("pluginFactory").textValue();
+                String instanceId = node.get("instanceId").textValue();
+                JsonNode configuration = node.get("configuration");
+
+                PluginConfiguration config = toPluginConfiguration(pluginFactory, instanceId, configuration, configuration);
+                pluginConfigurations.add(config);
+            }
+            return pluginConfigurations;
+        }
+
+        protected static PluginConfiguration toPluginConfiguration(String pluginFactory, String instanceId, JsonNode body, JsonNode configuration) throws IllegalArgumentException, ClassNotFoundException {
+            if (configuration == null) {
+                throw new IllegalArgumentException("payload needs a configuration node where the filter configuration is in");
+            }
+
+            Long priority = Long.valueOf(getContent(body, "priority", "0"));
+            PluginState state = PluginState.valueOf(getContent(body, "state", PluginState.ENABLED.name()));
+
+            return new PluginConfiguration(new ConfigurationId(pluginFactory, instanceId), priority, state, 1L, configuration);
+        }
+
+        private static String getContent(JsonNode body, String fieldname, String alternative) {
+            JsonNode fn = body.get(fieldname);
+            return fn == null ? alternative : fn.asText();
+        }
+
+        private static void assertArrayElement(JsonNode element, String fieldName, Class<? extends JsonNode> clazz) {
+            if (!(element instanceof ObjectNode)) {
+                throw new IllegalArgumentException("Array element is not an Object");
+            }
+
+            JsonNode field = element.get(fieldName);
+
+            if (!field.getClass().equals(clazz)) {
+                throw new IllegalArgumentException(format("Array element's contents does not contain %s as a %s class", fieldName, clazz));
+            }
+
+            if (field instanceof TextNode && !StringUtils.hasText(field.textValue())) {
+                throw new IllegalArgumentException(format("Array element's contents contains field %s but it contains an empty value", fieldName));
+            }
+        }
+    }
+}
---
+++ core-runtime/src/test/java/com/ecg/replyts/core/webapi/configv2/ConfigApiJsonMapperTest.java
@@ -0,0 +1,42 @@
+package com.ecg.replyts.core.webapi.configv2;
+
+import com.ecg.replyts.core.api.configadmin.ConfigurationId;
+import com.ecg.replyts.core.api.configadmin.PluginConfiguration;
+import com.ecg.replyts.core.api.pluginconfiguration.PluginState;
+import com.ecg.replyts.core.api.util.JsonObjects;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConfigApiJsonMapperTest {
+
+    @Test
+    public void properlyMapsModelToJson() {
+        List<PluginConfiguration> configList = Collections.singletonList(
+                new PluginConfiguration(
+                        new ConfigurationId("filterFactory", "fooinstance"),
+                        222L, PluginState.ENABLED, 12L, JsonObjects.parse("{foo: 322}")
+                )
+        );
+
+        // execute SUT
+        ObjectNode listConfigurations = ConfigApiJsonMapper.Model.toJson(configList);
+
+        ArrayNode configs = (ArrayNode) listConfigurations.get("configs");
+        ObjectNode firstConfig = (ObjectNode) configs.get(0);
+
+        assertEquals(1, configs.size());
+        assertEquals("ENABLED", firstConfig.get("state").textValue());
+        assertEquals(12L, firstConfig.get("version").longValue());
+        assertEquals(222L, firstConfig.get("priority").longValue());
+        assertEquals("filterFactory", firstConfig.get("pluginFactory").textValue());
+        assertEquals("fooinstance", firstConfig.get("instanceId").textValue());
+        assertEquals(322, firstConfig.get("configuration").get("foo").intValue());
+    }
+
+}
