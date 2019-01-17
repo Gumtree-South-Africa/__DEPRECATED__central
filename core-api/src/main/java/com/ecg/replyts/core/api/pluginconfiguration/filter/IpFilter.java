4b6029073eb6c24acec09e47ad4de89858d003c2
Alexey Kobyakov <akobiakov@ebay.com>
2019-01-17 16:17:20 +0100

    COMAAS-1686 - check for a different key in case of PMA (#1030)
    There's a big confusion between headers and metadata. I address this in COMAAS-1695.
    This change should at least unblock the MoVe marketing launch.

--- core-api/src/main/java/com/ecg/replyts/core/api/pluginconfiguration/filter/IpFilter.java
+++ core-api/src/main/java/com/ecg/replyts/core/api/pluginconfiguration/filter/IpFilter.java
@@ -1,13 +1,22 @@
 package com.ecg.replyts.core.api.pluginconfiguration.filter;
 
+import com.ecg.replyts.core.api.model.conversation.MessageTransport;
 import com.ecg.replyts.core.api.model.mail.Mail;
 import com.ecg.replyts.core.api.processing.MessageProcessingContext;
 
+
 public interface IpFilter extends Filter {
-    default String getIp(MessageProcessingContext context, String headerName) {
-        return context.getMail()
+    String IP_ADDRESS_METADATA_KEY = "ip-address";
+
+    // TODO akobiakov: the code below should go away as a whole: https://jira.corp.ebay.com/browse/COMAAS-1695
+    default String getIp(MessageProcessingContext ctx, String headerName) {
+        if (ctx.getTransport() == MessageTransport.CHAT
+                && ctx.getMessage().getCaseInsensitiveHeaders().containsKey(IP_ADDRESS_METADATA_KEY)) {
+            return ctx.getMessage().getCaseInsensitiveHeaders().get(IP_ADDRESS_METADATA_KEY);
+        }
+        return ctx.getMail()
                 .map(Mail::getUniqueHeaders)
-                .orElseGet(() -> context.getMessage().getCaseInsensitiveHeaders())
+                .orElseGet(() -> ctx.getMessage().getCaseInsensitiveHeaders())
                 .get(headerName);
     }
 }
